cdd304208769261e0a302dd0fed8e00257957c36
Manuel Dalla Lana <endelwar@aregar.it>
2014-01-13 17:21:15 +0100

    better logout process

--- mailscanner/logout.php
+++ mailscanner/logout.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 /*
  MailWatch for MailScanner
@@ -20,12 +20,24 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-require_once("./functions.php");
 session_start();
-require('login.function.php');
+// reset session variables
+$_SESSION = array();
 
-session_unset();
+// delete the session cookie.
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
 session_destroy();
-header("Location: index.php");
 
-dbclose();
+header("Location: index.php");
