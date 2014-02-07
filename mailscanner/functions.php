18b80894746d67d6ec06a0ad7b53346fa6eeaff4
Phil Randal <phil.randal@gmail.com>
2014-02-07 17:27:19 +0000

    Update functions.php
    Fix for PHP Version < 5.3

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -21,7 +21,12 @@
 */
 
 // Set error level (some distro's have php.ini set to E_ALL)
-error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+if (version_compare(phpversion(), '5.3.0', '<')) {
+  // E_DEPRECATED added in PHP 5.3
+  error_reporting(E_ALL ^ E_NOTICE);
+} else {
+  error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
+}
 
 // Read in MailWatch configuration file
 if(!(@include_once('conf.php'))==true) {
