2b4ba6d749296598e188f37eea3c87d9beb49fba
Phil Randal <phil.randal@gmail.com>
2014-07-15 21:11:10 +0100

    fix typo

--- mailscanner/rep_message_listing.php
+++ mailscanner/rep_message_listing.php
@@ -37,7 +37,7 @@ $sql = "
  SELECT
   id AS id2,
   DATE_FORMAT(timestamp, '" . DATE_FORMAT . " " . TIME_FORMAT . "') AS datetime,
-  from_address,"
+  from_address,";
 if (DISPLAY_IP) {
     $sql .= "clientip,";
 }
