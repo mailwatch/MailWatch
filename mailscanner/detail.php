7d66829ee440ee568596f01fa8077725d0f4ef91
Phil Randal <phil.randal@gmail.com>
2014-07-16 15:24:24 +0100

    fix typo
    Oops!

--- mailscanner/detail.php
+++ mailscanner/detail.php
@@ -202,7 +202,7 @@ while ($row = mysql_fetch_array($result, MYSQL_BOTH)) {
             $row[$f] = format_mail_size($row[$f]);
         }
         if ($fieldn == "Message Headers:") {
-            if (version_compare(phpversion(), "5.4", ">=") {
+            if (version_compare(phpversion(), "5.4", ">=")) {
                 $row[$f] = nl2br(
                     str_replace(array("\n", "\t"), array("<br>", "&nbsp; &nbsp; &nbsp;"), htmlentities($row[$f], ENT_COMPAT | ENT_HTML401 | ENT_SUBSTITUTE))
                 );
