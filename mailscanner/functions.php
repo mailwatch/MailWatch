5c57a9b4cf811b34fb30665d594fd100867fe164
Manuel Dalla Lana <endelwar@aregar.it>
2015-04-27 09:38:14 +0200

    remove E_STRICT from error reporting

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -41,7 +41,7 @@ if (version_compare(phpversion(), '5.3.0', '<')) {
     error_reporting(E_ALL);
 } else {
     // E_DEPRECATED added in PHP 5.3
-    error_reporting(E_ALL ^ E_DEPRECATED);
+    error_reporting(E_ALL ^ E_DEPRECATED ^ E_STRICT);
 }
 
 // Read in MailWatch configuration file
