eea462582db800b566c5961ce9b4e87a42c739a8
Rafael Mentz Aquino <rafael@lk6.com.br>
2015-06-18 12:00:03 -0300

    Create pt_br.php
    Brazilian Portuguese Translation (partial)

---
+++ mailscanner/languages/pt_br.php
@@ -0,0 +1,102 @@
+<?php
+
+// 01-login.php
+define('USERNAME01','Usu&aacute;rio');
+define('PASSWORD01','Senha');
+
+// 02-status.php
+$RECENTMESSAGES01="Mensagens Recentes";
+
+// 03-functions.php
+$MESSAGE01_03="Cannot read conf.php - please create it by copying conf.php.example and modifying the parameters to suit.";
+define('JUMPMESSAGE03','Carregar a mensagem:');
+define('CUSER03','Usu&aacute;rio');
+define('CST03','Hora do Sistema');
+define('COLORCODES03','C&oacute;digo de Cores');
+define('CCODES01_03','Conte&uacute;do Bloq./Infectado');
+define('CCODES02_03','Lista Branca');
+define('CCODES03_03','Lista Negra');
+define('CCODES04_03','N&atilde;o Verificado');
+define('CCODES05_03','Limpo');
+define('FREEDSPACE03','Espaço em Disco Livre');
+define('TODAYSTOTALS03','Totais do Dia');
+define('PROCESSED03','Processados');
+define('CLEAN03','Limpos');
+define('VIRUSES03','Virus');
+define('BLOCKEDFILES03','Arquivos Bloqueados');
+define('OTHERS03','Outros');
+define('HSCOSPAM03','Spam Pont. Alta');
+define('HSCOMCP03','MCP Pont. Alta');
+define('RECENTMESSAGES03','Mensagens Recentes');
+define('LISTS03','Listas');
+define('QUARANTINE03','Quarantena');
+define('DATETIME03','Data/Hora');
+define('FROM03','De');
+define('TO03','Para');
+define('SIZE03','Tamanho');
+define('SUBJECT03','Assunto');
+define('SASCORE03','Pontos SA');
+define('MCPSCORE03','Pontos MCP');
+
+// 04-detail.php
+define('RECEIVEDON04','Recebido em');
+define('RECEIVEDBY04','Recebido por');
+define('RECEIVEDFROM04','Recebido de');
+define('RECEIVEDVIA04','Recebido Via');
+define('MSGHEADERS04','Cabe&ccedil;alhos');
+define('FROM04','De');
+define('TO04','Para');
+define('SIZE04','Tamanho');
+define('SUBJECT04','Assunto');
+define('HDRANTIVIRUS04','Anti-Virus/Prote&ccedil;&atilde;o de Conte&uacute;do Perigoso');
+define('BLKFILE04','Arquivo Bloqueado');
+define('OTHERINFEC04','Outra Infec&ccedil;&atilde;o');
+define('HSCOSPAM04','SPAM Pont. Alta');
+define('LISTEDRBL04','Listado em RBL');
+define('SPAMWL04','Lista Branca de SPAM');
+define('SPAMBL04','Lista Negra de SPAM');
+define('SAAUTOLEARN04','Autoaprendizando do Spamassassin');
+define('SASCORE04','Pontua&ccedil;&atilde;o do Spamassassin');
+define('SPAMREP04','Relat&oacute;rio de Spam');
+define('HDRMCP04','Prote&ccedil;&atilde;o de Conte&uacute;do de Mensagens (MCP)');
+define('HIGHSCOMCP04','MCP Pont. Alta');
+define('MCPWL04','Lista Branca de MCP');
+define('MCPBL04','Lista Negra de MCP');
+define('MCPSCORE04','Pontua&ccedil;&atilde;o do MCP');
+define('MCPREP04','Relat&oacute;rio MCP');
+define('IPADDRESS04','Endere&ccedil;o IP');
+define('COUNTRY04','Pa&iacute;s');
+define('ALL04','Tudo');
+define('ADDWL04','Adicionar na Lista Branca');
+define('ADDBL04','Adicionar na Lista Negra');
+define('RELEASE04','Liberar');
+define('DELETE04','Excluir');
+define('SALEARN04','Ensinar SA');
+define('FILE04','Arquivo');
+define('TYPE04','Tipo');
+define('PATH04','Caminho');
+define('DANG04','Perigoso');
+define('ALTRECIP04','Destinat&aacute;rio(s) Alternativo(s)');
+define('SUBMIT04','Enviar');
+define('ACTIONS04','A&ccedil;&atilde;o');
+define('QUARCMDRES04','Resultado da A&ccedil;&atilde;o da Quarentena');
+define('RESULTMSG04','Resultados');
+
+// 05-status.php
+define('LAST05','&Uacute;ltimas');
+define('MESSAGES05','Mensagens');
+define('REFEVERY05','Recarregando a cada');
+define('SECONDS05','segundos');
+
+// 06 - viewmail.php
+
+define('MSGVIEWER06','Visualiza&ccedil;&atilde;o da Mensagem');
+define('RELEASEMSG06','Liberar esta mensagem');
+define('DELETEMSG06','Excluir esta mensagem');
+define('ACTIONS06','A&ccedil;&otilde;es');
+define('DATE06','Data:');
+define('FROM06','De:');
+define('TO06','Para:');
+define('SUBJECT06','Assunto:');
+
+?>
