9bbf2a20e55ec6880dafc326ebf3c595579f0740
stefaweb <sleclerc@actionweb.fr>
2015-06-19 17:28:20 +0200

    French languages
    v0.0.1

---
+++ mailscanner/languages/fr.php
@@ -0,0 +1,105 @@
+<?php
+
+/* language/fr.php */
+/* v0.0.1 */
+
+$lang = array(
+	// 01-login.php
+	'USERNAME01' => 'Utilisateur',
+	'PASSWORD01' => 'Mot de passe',
+	// 03-functions.php
+	'JUMPMESSAGE03' => 'Aller au message :',
+	'CUSER03' => 'Utilisateur',
+	'CST03' => 'Heure système',
+	'COLORCODES03' => 'Codes couleur',
+	'CCODES01_03' => 'Contenu dangereux/Infecté',
+	'CCODES02_03' => 'Liste Blanche',
+	'CCODES03_03' => 'Liste Noire',
+	'CCODES04_03' => 'Non vérifié',
+	'CCODES05_03' => 'Bon',
+	'FREEDSPACE03' => 'Espace disque libre',
+	'TODAYSTOTALS03' => 'Total aujourd\'hui',
+	'PROCESSED03' => 'Traité',
+	'CLEAN03' => 'Bon',
+	'VIRUSES03' => 'Virussé',
+	'BLOCKEDFILES03' => 'Fichier bloqué',
+	'OTHERS03' => 'Otherd',
+	'HSCOSPAM03' => 'Spam à score très haut',
+	'HSCOMCP03' => 'MCP à score très haut',
+	'RECENTMESSAGES03' => 'Messages récent',
+	'LISTS03' => 'Listes',
+	'QUARANTINE03' => 'Quarantaine',
+	'DATETIME03' => 'Date/Heure',
+	'FROM03' => 'De',
+	'TO03' => 'A',
+	'SIZE03' => 'Taille',
+	'SUBJECT03' => 'Sujet',
+	'SASCORE03' => 'Score SA',
+	'MCPSCORE03' => 'Score MCP',
+	// 04-detail.php
+	'RECEIVEDON04' => 'Reçu le',
+	'RECEIVEDBY04' => 'Reçu par',
+	'RECEIVEDFROM04' => 'Reçu de',
+	'RECEIVEDVIA04' => 'Reçu via',
+	'MSGHEADERS04' => 'Headers du message',
+	'FROM04' => 'De',
+	'TO04' => 'A',
+	'SIZE04' => 'Taille',
+	'SUBJECT04' => 'Sujet',
+	'HDRANTIVIRUS04' => 'Anti-Virus/Protection contre les fichiers dangeureux',
+	'BLKFILE04' => 'Fichier bloqué',
+	'OTHERINFEC04' => 'Autre infection',
+	'OTHERINFEC04' => 'Autre infection',
+	'HSCOSPAM04' => 'Spam à score très haut',
+	'LISTEDRBL04' => 'Listé en RBL',
+	'SPAMWL04' => 'SPAM en Liste Blanche',
+	'SPAMBL04' => 'SPAM en Liste Noire',
+	'SAAUTOLEARN04' => 'Auto aprentissage Spamassassin',
+	'SASCORE04' => 'Spamassassin Score',
+	'SPAMREP04' => 'Rapport de Spam',
+	'HDRMCP04' => 'Message Content Protection (MCP)',
+	'HIGHSCOMCP04' => 'High Scoring MCP',
+	'MCPWL04' => 'MCP en Liste Blanche',
+	'MCPBL04' => 'MCP en Liste Noire',
+	'MCPSCORE04' => 'Score MCP',
+	'MCPREP04' => 'Rapport MCP',
+	'IPADDRESS04' => 'Adresse IP',
+	'COUNTRY04' => 'Pays',
+	'ALL04' => 'Tous',
+	'ADDWL04' => 'Ajouter à la Liste Blanche',
+	'ADDBL04' => 'Ajouter à la Liste Noire',
+	'RELEASE04' => 'Libérer',
+	'DELETE04' => 'Supprimer',
+	'SALEARN04' => 'Apprentissage SA',
+	'FILE04' => 'Fichier',
+	'TYPE04' => 'Type',
+	'PATH04' => 'Chemin',
+	'DANG04' => 'Dangereux',
+	'ALTRECIP04' => 'Destinataire alternatif',
+	'SUBMIT04' => 'Soumettre',
+	'ACTIONS04' => 'Action(s)',
+	'QUARCMDRES04' => 'Quarantine Command Results',
+	'RESULTMSGS04' => 'Result Messages',
+	// 05-status.php
+	'LAST05' => 'Derniers',
+	'MESSAGES05' => 'Messages',
+	'REFEVERY05' => 'Rafraichir tous les',
+	'SECONDS05' => 'segunds',
+	// 06 - viewmail.php
+	'MSGVIEWER06' => 'Message Viewer',
+	'RELEASEMSG06' => 'Libérer ce message',
+	'DELETEMSG06' => 'Supprimer ce message',
+	'ACTIONS06' => 'Actions',
+	'DATE06' => 'Date :',
+	'FROM06' => 'De :',
+	'TO06' => 'A :',
+	'SUBJECT06' => 'Sujet :',    
+    'cannot_read_conf' => 'Impossible de lire le fichier conf.php - Créez un fichier de configuration conf.php.example et modifiez les paramètres de configuration comme nécessaire.'
+    'i18_missing' => 'Non traduit en français'
+);
+
+return $lang;
+
+
+
+?>
