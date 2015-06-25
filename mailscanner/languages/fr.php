dc76335357d2716caa37731f14e71647282fd78a
stefaweb <sleclerc@actionweb.fr>
2015-06-25 08:17:46 +0200

    fr.php 0.1.6
    Please let in the line with th file version number.

--- mailscanner/languages/fr.php
+++ mailscanner/languages/fr.php
@@ -1,12 +1,45 @@
 <?php
+/*
+ MailWatch for MailScanner
+ Copyright (C) 2003-2011  Steve Freegard (steve@freegard.name)
+ Copyright (C) 2011  Garrod Alwood (garrod.alwood@lorodoes.com)
+ Copyright (C) 2014-2015  MailWatch Team (https://github.com/orgs/mailwatch/teams/team-stable)
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ In addition, as a special exception, the copyright holder gives permission to link the code of this program
+ with those files in the PEAR library that are licensed under the PHP License (or with modified versions of those
+ files that use the same license as those files), and distribute linked combinations including the two.
+ You must obey the GNU General Public License in all respects for all of the code used other than those files in the
+ PEAR library that are licensed under the PHP License. If you modify this program, you may extend this exception to
+ your version of the program, but you are not obligated to do so.
+ If you do not wish to do so, delete this exception statement from your version.
+
+ As a special exception, you have permission to link this program with the JpGraph library and
+ distribute executables, as long as you follow the requirements of the GNU GPL in regard to all of the software
+ in the executable aside from JpGraph.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 /* languages/fr.php */
-/* v0.1.5 */
+/* v0.1.6 */
 
-$lang = array(
+return array(
 	// 01-login.php
 	'username01' => 'Utilisateur',
 	'password01' => 'Mot de passe',
+	'cannot_read_conf' => 'Impossible de lire le fichier conf.php - Créez un fichier de configuration conf.php.example et modifiez les paramètres de configuration comme nécessaire.'
 	
 	// 03-functions.php
 	'jumpmessage03' => 'Aller au message :',
@@ -37,6 +70,7 @@ $lang = array(
 	'subject03' => 'Sujet',
 	'sascore03' => 'Score SA',
 	'mcpscore03' => 'Score MCP',
+	'found03' => 'trouvé',
 	
 	// 04-detail.php
 	'receivedon04' => 'Reçu le :',
@@ -83,6 +117,7 @@ $lang = array(
 	'resultmsgs04' => 'Résultat du message',
 	
 	// 05-status.php
+	'recentmsg05' => 'Messages récents',
 	'last05' => 'Derniers',
 	'messages05' => 'Messages',
 	'refevery05' => 'Rafraichir toutes les',
@@ -197,9 +232,5 @@ $lang = array(
 	'errorpass12' => 'Le mot de passe de correspond pas.',
 	
 	// 99 - General
-	'i18_missing' => 'Non traduit en français', // To code - Printed if i18 item is missing in the coresponding language.
-	'cannot_read_conf' => 'Impossible de lire le fichier conf.php - Créez un fichier de configuration conf.php.example et modifiez les paramètres de configuration comme nécessaire.'
-
-	);
-
-return $lang;
+	'i18_missing' => 'Non traduit en français',
+);
