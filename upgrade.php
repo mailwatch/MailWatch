5df235a17487c3f13b403e439bd1d1b2c6fdc386
Manuel Dalla Lana <endelwar@aregar.it>
2015-07-31 11:51:35 +0200

    Update upgrade script with new schema

--- upgrade.php
+++ upgrade.php
@@ -36,7 +36,17 @@ require("/var/www/html/mailscanner/functions.php");
 function pad($input)
 {
     $input = str_pad($input, 70, ".", STR_PAD_RIGHT);
+
     return $input;
+}
+
+function executeQuery($sql) {
+    if (@mysql_query($sql)) {
+        echo " OK\n";
+    } else {
+        echo " ERROR\n";
+        die("MySQL error: " . @mysql_error());
+    }
 }
 
 // Test connectivity to the database
@@ -47,72 +57,100 @@ if (($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS)) && @mysql_select_db(DB_N
     echo "Updating database schema: \n";
 
     /*
-    ** Updates to the schema for 1.0.3
+    ** Updates to the schema for 1.2.0
     */
+
+    echo pad(" - Convert database to UTF-8");
+    $sql = "ALTER DATABASE `" . DB_NAME . "` CHARACTER SET = utf8 COLLATE = utf8_unicode_ci";
+    executeQuery($sql);
+
+    $utf8_tables = array(
+        "audit_log",
+        "blacklist",
+        "inq",
+        "maillog",
+        "mcp_rules",
+        "mtalog",
+        "outq",
+        "sa_rules",
+        "saved_filters",
+        "spamscores",
+        "user_filters",
+        "users",
+        "whitelist",
+    );
+
+    foreach ($utf8_tables as $table => $utf8_query) {
+        echo pad(" - Convert table `" . $table . "` to UTF-8");
+        $sql = "ALTER TABLE `" . $table . "` CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci";
+        executeQuery($sql);
+    }
+
+    echo pad(" - Enlarge username field in `audit_log` table");
+    $sql = "ALTER TABLE `audit_log` CHANGE `user` `user` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
+    executeQuery($sql);
+
+    echo pad(" - Enlarge password field in `users` table");
+    $sql = "ALTER TABLE `users` CHANGE `password` `password` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
+    executeQuery($sql);
+
+    echo pad(" - Enlarge username field in `users` table");
+    $sql = "ALTER TABLE `users` CHANGE `username` `username` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
+    executeQuery($sql);
+
+    echo pad(" - Enlarge fullname field in `users` table");
+    $sql = "ALTER TABLE `users` CHANGE `fullname` `fullname` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
+    executeQuery($sql);
+
+    echo pad(" - Enlarge username field in `user_filters` table");
+    $sql = "ALTER TABLE `user_filters` CHANGE `username` `username` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
+    executeQuery($sql);
+
+    echo pad(" - Enlarge user field in `spamscore` table");
+    $sql = "ALTER TABLE `spamscore` CHANGE `user` `user` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''";
+    executeQuery($sql);
+
+    echo pad(" - Drop `geoip_country` table");
+    $sql = "DROP TABLE `geoip_country`";
+    executeQuery($sql);
+
+    /*
+    ** Updates to the schema for 1.0.3
+    *
 
     echo pad(" - Adding hostname column to inq table ");
     $sql = "ALTER TABLE inq ADD COLUMN (hostname TEXT)";
-    if (@mysql_query($sql)) {
-        echo " OK\n";
-    } else {
-        echo " ERROR\n";
-        die("MySQL error: " . @mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Adding hostname column to outq table ");
     $sql = "ALTER TABLE outq ADD COLUMN (hostname TEXT)";
-    if (@mysql_query($sql)) {
-        echo " OK\n";
-    } else {
-        echo " ERROR\n";
-        die("MySQL error: " . @mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Adding index to inq table ");
     $sql = "ALTER TABLE inq ADD INDEX inq_hostname (hostname(50))";
-    if (@mysql_query($sql)) {
-        echo " OK\n";
-    } else {
-        echo " ERROR\n";
-        die("MySQL error: " . @mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Adding index to outq table ");
     $sql = "ALTER TABLE outq ADD INDEX outq_hostname (hostname(50))";
-    if (@mysql_query($sql)) {
-        echo " OK\n";
-    } else {
-        echo " ERROR\n";
-        die("MySQL error: " . @mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating table spamscores ");
     $sql = "CREATE TABLE spamscores (
-  user varchar(40) NOT NULL default '',
-  lowspamscore decimal(10,0) NOT NULL default '0',
-  highspamscore decimal(10,0) NOT NULL default '0',
+  user VARCHAR(40) NOT NULL DEFAULT '',
+  lowspamscore DECIMAL(10,0) NOT NULL DEFAULT '0',
+  highspamscore DECIMAL(10,0) NOT NULL DEFAULT '0',
   PRIMARY KEY  (user)
 ) ENGINE=MyISAM";
-    if (@mysql_query($sql)) {
-        echo " OK\n";
-    } else {
-        echo " ERROR\n";
-        die("MySQL error: " . @mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Adding columns to the users table ");
     $sql = "ALTER TABLE users ADD COLUMN (
-  spamscore tinyint(4) default '0',
-  highspamscore tinyint(4) default '0',
-  noscan tinyint(1) default '0',
-  quarantine_rcpt varchar(60) default NULL
+  spamscore TINYINT(4) DEFAULT '0',
+  highspamscore TINYINT(4) DEFAULT '0',
+  noscan TINYINT(1) DEFAULT '0',
+  quarantine_rcpt VARCHAR(60) DEFAULT NULL
 )";
-    if (@mysql_query($sql)) {
-        echo " OK\n";
-    } else {
-        echo " ERROR\n";
-        die("MySQL error: " . @mysql_error());
-    }
+    executeQuery($sql);
 
     /*
     **  Updates to the schema for 1.0
@@ -123,29 +161,19 @@ if (($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS)) && @mysql_select_db(DB_N
     $sql = "DROP TABLE maillog_pre";
     @mysql_query($sql); // Don't care if this succeeds or not
     $sql = "CREATE TABLE maillog_pre AS SELECT * FROM maillog";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     // Drop the indexes
     echo pad(" - Dropping indexes on maillog table");
     $sql = "SHOW INDEX FROM maillog";
     $result = dbquery($sql);
-    while($row=mysql_fetch_object($result)) {
-     // Use key name as array key to remove dups...
-     $indexes[$row->Key_name] = true;
+    while ($row = mysql_fetch_object($result)) {
+        // Use key name as array key to remove dups...
+        $indexes[$row->Key_name] = true;
     }
     $drop_index = join(', DROP INDEX ', array_keys($indexes));
     $sql = "ALTER TABLE maillog DROP INDEX $drop_index";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     // Alter the database structure
 
@@ -163,21 +191,11 @@ if (($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS)) && @mysql_select_db(DB_N
     to_domain TEXT,
     isfn TINYINT(1) DEFAULT '0',
     issamcp TINYINT(1) DEFAULT '0')";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Populating from_domain and to_domain column ");
     $sql = "UPDATE maillog SET timestamp=timestamp, from_domain=SUBSTRING_INDEX(from_address,'@',-1), to_domain=SUBSTRING_INDEX(to_address,'@',-1)";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating indexes ");
     $sql = "
@@ -192,185 +210,120 @@ if (($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS)) && @mysql_select_db(DB_N
     ADD INDEX maillog_to_domain_idx (to_domain(50)),
     ADD INDEX maillog_quarantined_idx (quarantined)
    ";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Dropping 'relay' table ");
     $sql = "DROP TABLE relay";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Altering 'user_filters' table ");
-    $sql = "ALTER TABLE user_filters CHANGE COLUMN username username varchar(60) NOT NULL default ''";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    $sql = "ALTER TABLE user_filters CHANGE COLUMN username username VARCHAR(60) NOT NULL DEFAULT ''";
+    executeQuery($sql);
 
     echo pad(" - Altering 'users' table (1) ");
-    $sql = "ALTER TABLE users CHANGE COLUMN username username varchar(60) NOT NULL default ''";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    $sql = "ALTER TABLE users CHANGE COLUMN username username VARCHAR(60) NOT NULL DEFAULT ''";
+    executeQuery($sql);
 
     echo pad(" - Altering 'users' table (2) ");
-    $sql = "ALTER TABLE users CHANGE COLUMN type type enum('A','D','U','R','H') default 'U'";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    $sql = "ALTER TABLE users CHANGE COLUMN type type ENUM('A','D','U','R','H') DEFAULT 'U'";
+    executeQuery($sql);
 
     echo pad(" - Altering 'users' table (3) ");
     $sql = "ALTER TABLE users ADD COLUMN quarantine_report TINYINT(1) DEFAULT '0'";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating table 'audit_log' ");
     $sql = "
    CREATE TABLE audit_log (
-     timestamp timestamp NOT NULL,
-     user varchar(20) NOT NULL default '',
-     ip_address varchar(15) NOT NULL default '',
-     action text NOT NULL
+     timestamp TIMESTAMP NOT NULL,
+     user VARCHAR(20) NOT NULL DEFAULT '',
+     ip_address VARCHAR(15) NOT NULL DEFAULT '',
+     action TEXT NOT NULL
    ) ENGINE=MyISAM
    ";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating table 'blacklist' ");
     $sql = "
    CREATE TABLE blacklist (
-     id int(11) NOT NULL auto_increment,
-     to_address text,
-     to_domain text,
-     from_address text,
+     id INT(11) NOT NULL AUTO_INCREMENT,
+     to_address TEXT,
+     to_domain TEXT,
+     from_address TEXT,
      PRIMARY KEY  (id),
      UNIQUE KEY blacklist_uniq (to_address(100),from_address(100))
    ) ENGINE=MyISAM
    ";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating table 'geoip_country' ");
     $sql = "
    CREATE TABLE geoip_country (
-     begin_ip varchar(15) default NULL,
-     end_ip varchar(15) default NULL,
-     begin_num bigint(20) default NULL,
-     end_num bigint(20) default NULL,
-     iso_country_code char(2) default NULL,
-     country text,
+     begin_ip VARCHAR(15) DEFAULT NULL,
+     end_ip VARCHAR(15) DEFAULT NULL,
+     begin_num BIGINT(20) DEFAULT NULL,
+     end_num BIGINT(20) DEFAULT NULL,
+     iso_country_code CHAR(2) DEFAULT NULL,
+     country TEXT,
      KEY geoip_country_begin (begin_num),
      KEY geoip_country_end (end_num)
    ) ENGINE=MyISAM
    ";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating table 'mcp_rules' ");
     $sql = "
    CREATE TABLE mcp_rules (
-     rule char(100) NOT NULL default '',
-     rule_desc char(200) NOT NULL default '',
+     rule CHAR(100) NOT NULL DEFAULT '',
+     rule_desc CHAR(200) NOT NULL DEFAULT '',
      PRIMARY KEY  (rule)
    ) ENGINE=MyISAM
    ";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating table 'mtalog' ");
     $sql = "
    CREATE TABLE mtalog (
-     timestamp datetime default NULL,
-     host text,
-     type text,
-     msg_id varchar(20) default NULL,
-     relay text,
-     dsn text,
-     status text,
-     delay time default NULL,
+     timestamp DATETIME DEFAULT NULL,
+     host TEXT,
+     type TEXT,
+     msg_id VARCHAR(20) DEFAULT NULL,
+     relay TEXT,
+     dsn TEXT,
+     status TEXT,
+     delay TIME DEFAULT NULL,
      UNIQUE KEY mtalog_uniq (timestamp,host(10),type(10),msg_id,relay(20)),
      KEY mtalog_timestamp (timestamp),
      KEY mtalog_type (type(10))
    ) ENGINE=MyISAM
    ";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating table 'saved_filters' ");
     $sql = "
    CREATE TABLE saved_filters (
-     name text NOT NULL,
-     col text NOT NULL,
-     operator text NOT NULL,
-     value text NOT NULL,
-     username text NOT NULL,
+     name TEXT NOT NULL,
+     col TEXT NOT NULL,
+     operator TEXT NOT NULL,
+     value TEXT NOT NULL,
+     username TEXT NOT NULL,
      UNIQUE KEY unique_filters (name(20),col(20),operator(20),value(20),username(20))
    ) ENGINE=MyISAM
    ";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     echo pad(" - Creating table 'whitelist' ");
     $sql = "
    CREATE TABLE whitelist (
-     id int(11) NOT NULL auto_increment,
-     to_address text,
-     to_domain text,
-     from_address text,
+     id INT(11) NOT NULL AUTO_INCREMENT,
+     to_address TEXT,
+     to_domain TEXT,
+     from_address TEXT,
      PRIMARY KEY  (id),
      UNIQUE KEY whitelist_uniq (to_address(100),from_address(100))
    ) ENGINE=MyISAM
    ";
-    if(@mysql_query($sql)) {
-     echo " OK\n";
-    } else {
-     echo " ERROR\n";
-     die("MySQL error: ".@mysql_error());
-    }
+    executeQuery($sql);
 
     *
     ** Finished
