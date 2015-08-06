83753ee7c75212d35d6ad946449862d348a21f2b
Manuel Dalla Lana <endelwar@aregar.it>
2015-08-06 10:19:17 +0200

    added italian translation

---
+++ mailscanner/languages/it.php
@@ -0,0 +1,242 @@
+<?php
+
+/*
+ * MailWatch for MailScanner
+ * Copyright (C) 2003-2011  Steve Freegard (steve@freegard.name)
+ * Copyright (C) 2011  Garrod Alwood (garrod.alwood@lorodoes.com)
+ * Copyright (C) 2014-2015  MailWatch Team (https://github.com/orgs/mailwatch/teams/team-stable)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * In addition, as a special exception, the copyright holder gives permission to link the code of this program with
+ * those files in the PEAR library that are licensed under the PHP License (or with modified versions of those files
+ * that use the same license as those files), and distribute linked combinations including the two.
+ * You must obey the GNU General Public License in all respects for all of the code used other than those files in the
+ * PEAR library that are licensed under the PHP License. If you modify this program, you may extend this exception to
+ * your version of the program, but you are not obligated to do so.
+ * If you do not wish to do so, delete this exception statement from your version.
+ *
+ * As a special exception, you have permission to link this program with the JpGraph library and distribute executables,
+ * as long as you follow the requirements of the GNU GPL in regard to all of the software in the executable aside from
+ * JpGraph.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* languages/en.php */
+/* v0.0.1 */
+
+return array(
+    // 01-login.php
+    'username' => 'Utente',
+    'password' => 'Password',
+
+    // 03-funtions.php
+    'jumpmessage03' => 'Vai al messaggio:',
+    'cuser03' => 'Utente',
+    'cst03' => 'Orario del Sistema',
+    'colorcodes03' => 'Codici Colore',
+    'badcontentinfected03' => 'Contenuto Inappropriato/Infetto',
+    'whitelisted03' => 'Whitelist',
+    'blacklisted03' => 'Blacklist',
+    'notverified03' => 'Non Verificato',
+    'clean03' => 'Puliti',
+    'freedspace03' => 'Spazio Disco Libero',
+    'todaystotals03' => 'Totali di Oggi',
+    'processed03' => 'Elaborati',
+    'cleans03' => 'Puliti',
+    'viruses03' => 'Virus',
+    'blockedfiles03' => 'File Bloccati',
+    'others03' => 'Altri',
+    'hscospam03' => 'Alto Punteggio Spam',
+    'hscomcp03' => 'Alto Punteggio MCP',
+    'recentmessages03' => 'Messaggi Recenti',
+    'lists03' => 'Liste',
+    'quarantine03' => 'Quarantena',
+    'datetime03' => 'Data/Ora',
+    'from03' => 'Da',
+    'to03' => 'A',
+    'size03' => 'Dimensione',
+    'subject03' => 'Oggetto',
+    'sascore03' => 'Punteggio SA',
+    'mcpscore03' => 'Punteggio MPC',
+    'found03' => 'trovato',
+
+    // 04-details.php
+    'receivedon04' => 'Ricevuto il:',
+    'receivedby04' => 'Ricevuto su:',
+    'receivedfrom04' => 'Ricevuto da:',
+    'receivedvia04' => 'Ricevuto Tramite:',
+    'msgheaders04' => 'Intestazioni Messaggio:',
+    'from04' => 'Da:',
+    'to04' => 'A:',
+    'size04' => 'Dimensione:',
+    'subject04' => 'Oggetto:',
+    'hdrantivirus04' => 'Protezione AntiVirus/Contenuti Pericolosi',
+    'blkfile04' => 'File Bloccati:',
+    'otherinfec04' => 'Altre Infezioni:',
+    'hscospam04' => 'Alto Punteggio Spam:',
+    'listedrbl04' => 'Presente in RBL:',
+    'spamwl04' => 'SPAM in Whitelist:',
+    'spambl04' => 'SPAM in Blacklist:',
+    'saautolearn04' => 'Spamassassin Autolearn:',
+    'sascore04' => 'Punteggio Spamassassin:',
+    'spamrep04' => 'Report Spam:',
+    'hdrmcp04' => 'Protezione Contenuto Messaggio (MCP)',
+    'highscomcp04' => 'Alto Punteggio MCP:',
+    'mcpwl04' => 'MCP in Whitelist:',
+    'mcpbl04' => 'MCP in Blacklist:',
+    'mcpscore04' => 'Punteggio MPC:',
+    'mcprep04' => 'Report MCP:',
+    'ipaddress04' => 'Indirizzo IP',
+    'country04' => 'Paese',
+    'all04' => 'Tutti',
+    'addwl04' => 'Aggiungi a Whitelist',
+    'addbl04' => 'Aggiungi a Blacklist',
+    'release04' => 'Rilascia',
+    'delete04' => 'Elimina',
+    'salearn04' => 'SA Learn',
+    'file04' => 'File',
+    'type04' => 'Tipo',
+    'path04' => 'Percorso',
+    'dang04' => 'Pericoloso',
+    'altrecip04' => 'Destinatari Alternativi',
+    'submit04' => 'Invia',
+    'actions04' => 'Azioni',
+    'quarcmdres04' => 'Risultato Comando Quarantena',
+    'resultmsg04' => 'Risultati Comando',
+
+    // 05-status.php
+    'recentmsg05' => 'Messaggi Recenti',
+    'last05' => 'Ultimo',
+    'messages05' => 'Messaggi',
+    'refevery05' => 'Aggiornamento ogni',
+    'seconds05' => 'secondi',
+
+    // 06-viewmail.php
+    'msgviewer06' => 'Visualizzatore Messagio',
+    'releasemsg06' => 'Rilascia questo messaggio',
+    'deletemsg06' => 'Cancella questo messaggio',
+    'actions06' => 'Azioni',
+    'date06' => 'Data:',
+    'from06' => 'Da:',
+    'to06' => 'A:',
+    'subject06' => 'Oggetto:',
+
+    // Added in 2015-06-22
+
+    // 03-functions.php
+    'reports03' => 'Rapporti',
+    'toolslinks03' => 'Strumenti/Link',
+    'softwareversions03' => 'Versioni Software',
+    'documentation03' => 'Documentazione',
+    'logout03' => 'Disconnetti',
+    'pggen03' => 'Pagina generata in',
+    'seconds03' => 'secondi',
+    'disppage03' => 'Visualizzando pagina',
+    'of03' => 'da',
+    'records03' => 'Documenti',
+    'to0203' => 'a', // This means to as "from number x to number Y". The first to03 means "from mail ... to mail ..."
+
+    // 07-lists.php
+    'addwlbl07' => 'Aggiungi a Whitelist/Blacklist',
+    'from07' => 'Da',
+    'to07' => 'A',
+    'list07' => 'Lista',
+    'action07' => 'Azione',
+    'wl07' => 'Whitelist',
+    'bl07' => 'Blacklist',
+    'reset07' => 'Resetta',
+    'add07' => 'Aggiungi',
+    'delete07' => 'Elimina',
+
+    // 08-quarantine.php
+    'folder08' => 'Cartella',
+    'folder_0208' => 'Cartella',
+    'items08' => 'oggetti',
+
+    // 09-filter.inc
+    'activefilters09' => 'Filtri Attivi',
+    'none09' => 'Nessuno',
+    'addfilter09' => 'Aggiungi Filtro',
+    'loadsavef09' => 'Carica/Salva Filtri',
+    'tosetdate09' => 'Per impostare la data devi utilizzare il formato YYYY-mm-dd',
+
+    // Added in 2015-06-23
+    // 04-details.php
+    'quarantine04' => 'Quarantena',
+
+    // 03-functons.php
+    'score03' => 'Punteggio',
+    'matrule03' => 'Regola Corrispondente',
+    'description03' => 'Descrizione',
+
+    // 10-other.php
+    'tools10' => 'Strumenti',
+    'usermgnt10' => 'Gestione Utenti',
+    'viewconfms10' => 'Visualizza la Configurazione di MailScanner',
+    'editmsrules10' => 'Modifica Set di Regole di MailScanner',
+    'updatesadesc10' => 'Aggiorna Descrizione Regole SpamAssassin',
+    'updatemcpdesc10' => 'Aggiorna Descrizione Regole MCP',
+    'updategeoip10' => 'Aggiorna Database GeoIP',
+
+    // 11-sf_versions.php
+    'softver11' => 'Versioni Software',
+    'nodbdown11' => 'Nessun database scaricato',
+    'version11' => 'Versione:',
+
+    // 12-user_manager.php
+    'usermgnt12' => 'Gestore Utenti',
+    'username12' => 'Nome Utente',
+    'fullname12' => 'Nome Completo',
+    'type12' => 'Tipo',
+    'spamcheck12' => 'Controllo Spam',
+    'spamscore12' => 'Punteggio Spam',
+    'spamhscore12' => 'Alto Punteggio Spam',
+    'action12' => 'Azioni',
+    'edit12' => 'Modifica',
+    'delete12' => 'Elimina',
+    'filters12' => 'Filtri',
+    'newuser12' => 'Nuovo Utente',
+    'forallusers12' => 'Per tutti gli utenti diversi da Amministratore devi utilizzare un indirizzo email come nome utente',
+    'username0212' => 'Nome Utente:',
+    'name12' => 'Nome:',
+    'password12' => 'Password:',
+    'usertype12' => 'Tipo Utente:',
+    'user12' => 'Utente',
+    'domainadmin12' => 'Amministratore di Dominio',
+    'admin12' => 'Amministratore',
+    'quarrep12' => 'Report Quarantena:',
+    'senddaily12' => 'Invia Report Giornaliero?',
+    'quarreprec12' => 'Destinaratio Report Quarantena:',
+    'overrec12' => 'Sostituire il destinatario del report di quarantena?<BR>(verrà utilizzato il tuo nome utente, se vuoto)',
+    'scanforspam12' => 'Analizzare per Spam?',
+    'scaneforspam12' => 'Analizzare le e-mail per Spam?',
+    'pontspam12' => 'Punteggio Spam:',
+    'hpontspam12' => 'Alto Punteggio Spam:',
+    'usedefault12' => 'Utilizza Predefinito',
+    'action_0212' => 'Azione:',
+    'reset12' => 'Resetta',
+    'areusuredel12' => 'Sei sicuro di voler cancellare l\'utente',
+    'errorpass12' => 'Le Password non corrispondono',
+
+    // Added in 2015-06-25
+    // 03-funtions.php
+    'footer01' => 'MailWatch for MailScanner v',
+
+    // 08-quarantine.php
+    'input01' => 'Esegui ora',
+
+    // 13-sa_rules_update.php
+    'folder_0308' => 'Cartella di Quarantena',
+
+    // 99 - General
+    'i18_missing' => 'Non tradotto in Italiano',
+    'cannot_read_conf' => "Impossibile leggere il file conf.php - per favore crealo copiando il file conf.php.example e modifica i parametri necessari",
+);
