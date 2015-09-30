8674e763a9f6c6540c11e2fc92bba5dec9bfbb7a
Manuel Dalla Lana <endelwar@aregar.it>
2015-09-30 22:30:16 +0200

    Remove an unrequired dot in require path
    closes #232

--- mailscanner/viewpart.php
+++ mailscanner/viewpart.php
@@ -29,7 +29,7 @@
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-require_once(__DIR__ . './functions.php');
+require_once(__DIR__ . '/functions.php');
 require_once(__DIR__ . '/lib/pear/Mail/mimeDecode.php');
 
 session_start();
