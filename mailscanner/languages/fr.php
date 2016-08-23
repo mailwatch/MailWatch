01e89b71533e05cd3e2f2060a1e329b8177cc685
stefaweb <sleclerc@actionweb.fr>
2016-08-23 17:42:23 +0200

    geoip_update.php bis

--- mailscanner/languages/fr.php
+++ mailscanner/languages/fr.php
@@ -342,7 +342,7 @@ return array(
     'norread15' => 'Impossible de lire ou d\'écrire sur le',
     'message715' => 'Les fichiers existent toujours pour une raison inconnue.',
     'message815' => 'Effacez-les à la main à partir de',
-    'directory15' => 'répertoire'
+    'directory15' => 'répertoire',
             
     //auto-release.php
     'msgnotfound1' => 'Message non trouvé. Vous avez déjà du libérer ce message ou le lien n\'est peut être plus plus bon.',
