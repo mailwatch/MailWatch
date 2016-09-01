0dd94412ade7c2a4d17581c8a3283000375e4c6b
stefaweb <sleclerc@actionweb.fr>
2016-09-01 11:59:14 +0200

    auto-release.php sync with develop

--- mailscanner/auto-release.php
+++ mailscanner/auto-release.php
@@ -1,10 +1,39 @@
 <?php
 
 /*
+ * MailWatch for MailScanner
+ * Copyright (C) 2003-2011  Steve Freegard (steve@freegard.name)
+ * Copyright (C) 2011  Garrod Alwood (garrod.alwood@lorodoes.com)
+ * Copyright (C) 2014-2016  MailWatch Team (https://github.com/orgs/mailwatch/teams/team-stable)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * In addition, as a special exception, the copyright holder gives permission to link the code of this program with
+ * those files in the PEAR library that are licensed under the PHP License (or with modified versions of those files
+ * that use the same license as those files), and distribute linked combinations including the two.
+ * You must obey the GNU General Public License in all respects for all of the code used other than those files in the
+ * PEAR library that are licensed under the PHP License. If you modify this program, you may extend this exception to
+ * your version of the program, but you are not obligated to do so.
+ * If you do not wish to do so, delete this exception statement from your version.
+ *
+ * As a special exception, you have permission to link this program with the JpGraph library and distribute executables,
+ * as long as you follow the requirements of the GNU GPL in regard to all of the software in the executable aside from
+ * JpGraph.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
  * Mailwatch for Mailscanner Modification
  * Author: Alan Urquhart - ASU Web Services Ltd
- * Version: 1.1.1
- * Updated: 31-08-2016
+ * Version: 1.1
+ * Updated: 01-09-2016
  *
  * Requires: Mailwatch 1.2.0
  *
@@ -36,8 +65,8 @@ if (isset($_GET['mid']) && isset($_GET['r'])) {
         echo __('dberror59');
     }
     if (mysql_num_rows($result) == 0) {
-        echo "<p>". __('msgnotfound159')."</p>";
-        echo "<p>". __('msgnotfound259').$mid." ". __('msgnotfound359')."</p>";
+        echo "<p>" . __('msgnotfound159') . "</p>";
+        echo "<p>" . __('msgnotfound259') . htmlentities($mid) . " " . __('msgnotfound359') . "</p>";
     } else {
         $row = mysql_fetch_assoc($result);
         if ($row['uid'] == $token) {
@@ -56,7 +85,7 @@ if (isset($_GET['mid']) && isset($_GET['r'])) {
 
 
             // Display success
-            echo "<p>". __('msgreleased59'). "</p>";
+            echo "<p>" . __('msgreleased59') . "</p>";
             //cleanup
             $releaseID = $row['id'];
             $query = "DELETE FROM autorelease WHERE id = '$releaseID'";
