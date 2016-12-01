5b0d691175a04bffd6e3417a4b1198b5f4eb9a43
Manuel Dalla Lana <endelwar@aregar.it>
2016-12-01 10:52:27 +0100

    Make session cookie more secure
    closes #325

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -67,6 +67,14 @@ if (!is_file(__DIR__ . '/languages/' . LANG . '.php')) {
     $lang = require_once(__DIR__ . '/languages/' . LANG . '.php');
 }
 
+// more secure session cookies
+ini_set("session.use_cookies", 1);
+ini_set("session.use_only_cookies", 1);
+ini_set("session.use_trans_sid", 0);
+if (SSL_ONLY === true) {
+    ini_set("session.cookie_secure", 1);
+}
+
 if (PHP_SAPI !== 'cli' && SSL_ONLY && (!empty($_SERVER['PHP_SELF']))) {
     if (!$_SERVER['HTTPS'] == 'on') {
         header("Location: https://" . sanitizeInput($_SERVER['HTTP_HOST']) . sanitizeInput($_SERVER['REQUEST_URI']));
