f80f557d76499cd284d1c17eecf4c7091df97a8f
Manuel Dalla Lana <endelwar@aregar.it>
2016-12-03 01:35:04 +0100

    User safe_value in rpcserver.php

--- mailscanner/rpcserver.php
+++ mailscanner/rpcserver.php
@@ -95,7 +95,7 @@ function rpc_return_quarantined_file($msg)
     $input = php_xmlrpc_decode(array_shift($msg->params));
     $input = preg_replace('[\.\/|\.\.\/]', '', $input);
     $date = @database::mysqli_result(
-        dbquery("SELECT DATE_FORMAT(date,'%Y%m%d') FROM maillog where id='" . mysql_real_escape_string($input) . "'"),
+        dbquery("SELECT DATE_FORMAT(date,'%Y%m%d') FROM maillog where id='" . safe_value($input) . "'"),
         0
     );
     $qdir = get_conf_var('QuarantineDir');
