9f5fcc308b117c42468ab7192b0df45967c9e310
stefaweb <sleclerc@actionweb.fr>
2017-01-02 15:17:57 +0100

    fr.php udpate
    Typo in French fr.php file.

--- mailscanner/languages/fr.php
+++ mailscanner/languages/fr.php
@@ -730,7 +730,7 @@ return array(
     'title61' => 'Rapport de message en Quarantaine',
     'message61' => 'La variable $constant est vide. Veuillez définir cette variable dans conf.php.',
     'text611' => 'Rapport de Quarantaine pour %s',
-    'text612' => 'Durant le(s) %s jour(s) vous avez reçu %s messages qui ont été mis en Quarantaine et dont vous retrouvrez la liste ci-dessous. Les messages en Quarantaine sont automatiquement effacés %s jours après la date à laquelle ils ont été reçus.',
+    'text612' => 'Durant le(s) %s dernier jour(s) vous avez reçu %s messages qui ont été mis en Quarantaine et dont vous retrouverez la liste ci-dessous. Les messages en Quarantaine sont automatiquement effacés %s jours après la date à laquelle ils ont été reçus.',
 
     // 62-quarantine_maint.php
     'message62' => 'La variable $constant est vide. Veuillez définir cette variable dans conf.php.',
