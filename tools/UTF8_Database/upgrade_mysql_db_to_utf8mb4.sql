0052b4cfa7a744960eae188041179faed237b3df
Manuel Dalla Lana <endelwar@aregar.it>
2017-01-02 12:52:50 +0100

    Fix utf8 and utf8mb4 upgrade sql scripts

--- tools/UTF8_Database/upgrade_mysql_db_to_utf8mb4.sql
+++ tools/UTF8_Database/upgrade_mysql_db_to_utf8mb4.sql
@@ -5,51 +5,51 @@ REPAIR TABLE `audit_log`;
 OPTIMIZE TABLE `audit_log`;
 
 ALTER TABLE `blacklist` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `blacklist`;
+OPTIMIZE TABLE `blacklist`;
 
 ALTER TABLE `inq` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `inq`;
+OPTIMIZE TABLE `inq`;
 
 ALTER TABLE `maillog` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `maillog`;
+OPTIMIZE TABLE `maillog`;
 
 ALTER TABLE `mcp_rules` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `mcp_rules`;
+OPTIMIZE TABLE `mcp_rules`;
 
 ALTER TABLE `mtalog` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `mtalog`;
+OPTIMIZE TABLE `mtalog`;
 
 ALTER TABLE `outq` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `outq`;
+OPTIMIZE TABLE `outq`;
 
 ALTER TABLE `sa_rules` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `sa_rules`;
+OPTIMIZE TABLE `sa_rules`;
 
 ALTER TABLE `saved_filters` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `saved_filters`;
+OPTIMIZE TABLE `saved_filters`;
 
 ALTER TABLE `spamscores` CHANGE `user` `user` VARCHAR(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 ALTER TABLE `spamscores` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `spamscores`;
+OPTIMIZE TABLE `spamscores`;
 
 ALTER TABLE `user_filters` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `user_filters`;
+OPTIMIZE TABLE `user_filters`;
 
 ALTER TABLE `users` CHANGE `username` `username` VARCHAR(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 ALTER TABLE `users` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `users`;
+OPTIMIZE TABLE `users`;
 
 ALTER TABLE `whitelist` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-REPAIR TABLE `audit_log`;
-OPTIMIZE TABLE `audit_log`;
+REPAIR TABLE `whitelist`;
+OPTIMIZE TABLE `whitelist`;
