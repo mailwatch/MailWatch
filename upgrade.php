2c238a396839697631c1e3918e0a1d5372b8fbab
asuweb <alan@asuweb.co.uk>
2017-01-05 13:52:15 +0000

    Fixes #344 - upgrade.php now works with mysqli (#347)
    converted to use database class / pdo

--- upgrade.php
+++ upgrade.php
@@ -33,6 +33,7 @@
 header("Content-type: text/plain\n\n");
 require("/var/www/html/mailscanner/functions.php");
 
+$link = dbconn();
 function pad($input)
 {
     $input = str_pad($input, 70, ".", STR_PAD_RIGHT);
@@ -42,25 +43,27 @@ function pad($input)
 
 function executeQuery($sql)
 {
-    if (@mysql_query($sql)) {
+    global $link;
+    if ($link->query($sql)) {
         echo " OK\n";
     } else {
         echo " ERROR\n";
-        die("MySQL error: " . @mysql_error());
+        die("Database error: " . $link->error);
     }
 }
 
 function check_utf8_table($db, $table)
 {
+    global $link;
     $sql = 'SELECT c.character_set_name
             FROM information_schema.tables AS t, information_schema.collation_character_set_applicability AS c
             WHERE c.collation_name = t.table_collation
-            AND t.table_schema = "' . mysql_real_escape_string($db) . '"
-            AND t.table_name = "' . mysql_real_escape_string($table) . '"';
-    $result = @mysql_query($sql);
+            AND t.table_schema = "' . $link->real_escape_string($db) . '"
+            AND t.table_name = "' . $link->real_escape_string($table) . '"';
+    $result = $link->query($sql);
 
-    if (strtolower(mysql_result($result, 0)) === 'utf8') {
-        mysql_free_result($result);
+    if (strtolower(database::mysqli_result($result, 0)) === 'utf8') {
+        //mysqli_free_result($result);
 
         return true;
     }
@@ -70,15 +73,16 @@ function check_utf8_table($db, $table)
 
 function getTableIndexes($table)
 {
+    global $link;
     $sql = 'SHOW INDEX FROM `' . $table . '`';
-    $result = @mysql_query($sql);
+    $result = $link->query($sql);
 
     $indexes = array();
-    if (!$result || mysql_num_rows($result) === 0) {
+    if (!$result || $result->num_rows === 0) {
         return $indexes;
     }
 
-    while ($row = mysql_fetch_assoc($result)) {
+    while ($row = $result->fetch_assoc()) {
         $indexes[] = $row['Key_name'];
     }
 
@@ -89,7 +93,7 @@ $errors = false;
 
 // Test connectivity to the database
 echo pad("Testing connectivity to the database ");
-if (($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS)) && @mysql_select_db(DB_NAME)) {
+if ($link) {
     echo " OK\n";
     // Update schema at this point
     echo "Updating database schema: \n";
@@ -400,10 +404,10 @@ if (($link = @mysql_connect(DB_HOST, DB_USER, DB_PASS)) && @mysql_select_db(DB_N
     */
 
     // Phew! - finished
-    mysql_close($link);
+    dbclose();
 } else {
     echo " FAILED\n";
-    $errors[] = "Database connection failed: " . @mysql_error();
+    $errors[] = "Database connection failed: " . $link->error;
 }
 
 echo "\n";
