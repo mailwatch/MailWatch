ff3d7c477f5c692e72f1c09083564264bb38cfd7
Alan Urquhart <alan@asuweb.co.uk>
2017-01-07 10:18:57 +0000

    password reset function

--- mailscanner/password_reset.php
+++ mailscanner/password_reset.php
@@ -6,4 +6,418 @@
  * Web: www.asuweb.co.uk
  * Date: 07/01/2017
  * Time: 10:16
+ *
+ * Created for Mailwatch 1.2.0 RC4
  */
+require_once __DIR__ . '/functions.php';
+if (!QUARANTINE_USE_SENDMAIL) {
+    // Load in the required PEAR modules
+    require_once MAILWATCH_HOME . '/lib/pear/Mail.php';
+    require_once MAILWATCH_HOME . '/lib/pear/Mail/smtp.php';
+    require_once MAILWATCH_HOME . '/lib/pear/Mail/mime.php';
+    date_default_timezone_set(TIME_ZONE);
+}
+$showpage = false;
+$fields = '';
+$errors='';
+$message = '';
+$link = dbconn();
+function get_random_string($count)
+{
+    $bytes = openssl_random_pseudo_bytes($count);
+    return bin2hex($bytes);
+}
+
+if(defined('PWD_RESET') && PWD_RESET === true)
+{
+    if(isset($_POST['Submit']) && $_POST['Submit'] == 'Request Password Reset') {
+        //check email add registered user and password reset is allowed
+        $email = $link->real_escape_string($_POST['email']);
+        $sql = "SELECT * FROM users WHERE username = '$email'";
+        $result = dbquery($sql);
+        if ($result->num_rows != 1) {
+            //user not found
+            $errors = '<p class="error">'.__('notfound100').'</p>';
+            $showpage = true;
+        } else {
+            //user found, now check type of user
+            $row = $result->fetch_assoc();
+            if ($row['type'] == 'U') {
+                //user type is user, password reset allowed
+                $rand = get_random_string(10);
+                $resetexpire = time() + 60*60*RESET_LINK_EXPIRE;
+                $sql = "UPDATE users SET resetid = '$rand', resetexpire = '$resetexpire' WHERE username = '$email'";
+                $result = dbquery($sql);
+                if (!$result) die(__('errordbupdate100'));
+                $html = '<!DOCTYPE html>
+<html>
+<head>
+ <title>'.__('title100').'</title>
+ <style type="text/css">
+ <!--
+  body, td, tr {
+  font-family: sans-serif;
+  font-size: 8pt;
+ }
+ -->
+ </style>
+</head>
+<body style="margin: 5px;">
+
+<!-- Outer table -->
+<table width="100%%" border="0">
+ <tr>
+  <td><img src="'. IMAGES_DIR . MW_LOGO .'" alt="'.__('mwlogo99').'"/></td>
+  <td align="center" valign="middle">
+   <h2>'.__('h2email100').'</h2>
+   <p>'. __('p1email100') . $email .__('p2email100').'
+    <a href="' . QUARANTINE_REPORT_HOSTURL . '/password_reset.php?stage=2&user=' . $email . '&uid=' . $rand . '"><button>'.__('button100').'</button></a></p>
+  </td>
+ </tr>
+ </table>
+</body>
+</html>';
+                $text = __('01emailplaintxt100') . $email . __('02emailplaintxt100') . QUARANTINE_REPORT_HOSTURL . '/password_reset.php?stage=2&user=' . $email . '&uid=' . $rand;
+
+                //Send email
+                $mime = new Mail_mime("\n");
+                if(defined('PWD_RESET_FROM_NAME') && defined('PWD_RESET_FROM_ADDRESS') && PWD_RESET_FROM_NAME != '' && PWD_RESET_FROM_ADDRESS != '') {
+                    $sender = PWD_RESET_FROM_NAME . '<' . PWD_RESET_FROM_ADDRESS . '>';
+                }
+                else{
+                    $sender = QUARANTINE_REPORT_FROM_NAME . ' <' . QUARANTINE_FROM_ADDR . '>';
+                }
+                $hdrs = array(
+                    'From' => $sender,
+                    'To' => $email,
+                    'Subject' => __('01emailsubject100'),
+                    'Date' => date("r")
+                );
+                $mime->addHTMLImage(MAILWATCH_HOME . IMAGES_DIR . MW_LOGO, 'image/png', MW_LOGO, true);
+                $mime->setTXTBody($text);
+                $mime->setHTMLBody($html);
+                $body = $mime->get();
+                $hdrs = $mime->headers($hdrs);
+                $mail_param = array('host' => QUARANTINE_MAIL_HOST);
+                $mail =& Mail::factory('smtp', $mail_param);
+                $mail->send($email, $hdrs, $body);
+                $message = '<p>'.__('01emailsuccess100').'</p>';
+                $showpage = true;
+            }
+
+
+            else
+            {
+                //password reset not allowed
+                die(__('resetnotallowed100'));
+            }
+        }
+    }
+
+    else if(isset($_POST['Submit']) && $_POST['Submit'] == 'Reset Password') {
+        //check passwords match, update password in database, update password last changed date, increase password reset counter, email user to inform of password reset
+        $email = $link->real_escape_string($_POST['email']);
+        $uid = $link->real_escape_string($_POST['uid']);
+        if ($_POST['pwd1'] === $_POST['pwd2']) {
+            //passwords match, now we need to store them
+            //first, check form hasn't been modified
+            $sql = "SELECT resetid FROM users WHERE username = '$email'";
+            $result = dbquery($sql);
+            if(!$result)die("Error: ".$link->error);
+            $row = $result->fetch_array();
+            if($row['resetid'] == $_POST['uid'])
+            {
+                require_once(MAILWATCH_HOME . '/lib/password.php');
+                $password = $link->real_escape_string(password_hash($_POST['pwd1'], PASSWORD_DEFAULT));
+                $lastreset = time();
+                $sql = "UPDATE users SET password = '$password', resetid = '', resetexpire = '0', lastreset ='$lastreset' WHERE username ='$email'";
+                $result = dbquery($sql);
+                if (!$result) die(__('errorpwdchange100'));
+
+                //now send email telling user password has been updated.
+                $html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+ <title>'.__('01pwdresetemail100').'</title>
+ <style type="text/css">
+ <!--
+  body, td, tr {
+  font-family: sans-serif;
+  font-size: 8pt;
+ }
+ -->
+ </style>
+</head>
+<body style="margin: 5px;">
+
+<!-- Outer table -->
+<table width="100%%" border="0">
+ <tr>
+  <td><img src="'. IMAGES_DIR . MW_LOGO .'" alt="'.__('mwlogo99').'"/></td>
+  <td align="center" valign="middle">
+   <h2>'.__('02pwdresetemail100').'</h2>
+   <p>'.__('03pwdresetemail100'). ' ' . $email . ' ' . __('04pwdresetemail100') . '</p>
+  </td>
+ </tr>
+ </table>
+</body>
+</html>';
+                $text = __('05pwdresetemail100') . ' ' . $email . ' ' . __('06pwdresetemail100');
+
+                //Send email
+                $mime = new Mail_mime("\n");
+                if(defined('PWD_RESET_FROM_NAME') && defined('PWD_RESET_FROM_ADDRESS') && PWD_RESET_FROM_NAME != '' && PWD_RESET_FROM_ADDRESS != '') {
+                    $sender = PWD_RESET_FROM_NAME . '<' . PWD_RESET_FROM_ADDRESS . '>';
+                }
+                else{
+                    $sender = QUARANTINE_REPORT_FROM_NAME . ' <' . QUARANTINE_FROM_ADDR . '>';
+                }
+                $hdrs = array(
+                    'From' => $sender,
+                    'To' => $email,
+                    'Subject' => __('02emailsubject100'),
+                    'Date' => date("r")
+                );
+                $mime->addHTMLImage(MAILWATCH_HOME . IMAGES_DIR . MW_LOGO, 'image/png', MW_LOGO, true);
+                $mime->setTXTBody($text);
+                $mime->setHTMLBody($html);
+                $body = $mime->get();
+                $hdrs = $mime->headers($hdrs);
+                $mail_param = array('host' => QUARANTINE_MAIL_HOST);
+                $mail =& Mail::factory('smtp', $mail_param);
+                $mail->send($email, $hdrs, $body);
+                $message = '<p>' . __('pwdresetsuccess100') . '<br/>
+<a href="login.php"><button>' . __('login01') . '</button></a></p>';
+                $showpage = true;
+            }
+            else
+            {
+                die(__('pwdresetidmismatch'));
+            }
+        }
+        else
+        {
+            $errors = '<p>' . __('pwdmismatch');
+            $fields = "stage2";
+            $showpage = true;
+        }
+    }
+    else if(isset($_GET['stage']) && $_GET['stage'] == 1)
+    {
+        //first stage, need to get email address
+        $fields = 'stage1';
+        $showpage = true;
+    }
+    else if(isset($_GET['stage']) && $_GET['stage']==2) {
+        //need to check if reset allowed, and reset password
+        if (isset($_GET['user']) && isset($_GET['uid'])) {
+            //check that uid is correct
+            //dbconn();
+            $email = $link->real_escape_string($_GET['user']);
+            $uid = $link->real_escape_string($_GET['uid']);
+            $sql = "SELECT * FROM users WHERE username = '$email'";
+            $result = dbquery($sql);
+            if (!$result) die(__('errordb100') . $link->error);
+            if ($result->num_rows != '1') {
+                echo __('usernotfound100');
+            } else {
+                $row = $result->fetch_array();
+                if ($row['resetid'] === $uid) {
+                    //reset id matches - check if link expired
+                    if($row['resetexpire'] < time()) {
+                        echo __('resetexpired') . '<a href="password_reset.php?stage=1">'.__('button100') . '</a>';
+                    }
+                    else {
+                        $fields = "stage2";
+                        $showpage = true;
+                    }
+                } else {
+                    echo __('pwdresetidmismatch');
+                }
+
+            }
+        } else {
+            //no matches - deny
+            die(__('brokenlink100'));
+        }
+    }
+
+    if($showpage)
+    {
+        ?>
+        <!doctype html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>Password Reset Page</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="shortcut icon" href="images/favicon.png">
+            <style type="text/css">
+                body {
+                    background-color: #ffffff;
+                    color: #000;
+                    font-family: Arial, Helvetica, sans-serif;
+                    font-size: 16px;
+                    line-height: 1.5em;
+                }
+
+                .pwdreset {
+                    margin: 50px auto;
+                    width: 308px;
+                }
+
+                .pwdreset h1 {
+                    background-color: #f7ce4a;
+                    -webkit-border-radius: 15px 15px 0 0;
+                    -moz-border-radius: 15px 15px 0 0;
+                    border-radius: 15px 15px 0 0;
+                    color: #222;
+                    font-size: 28px;
+                    padding: 15px 15px;
+                    margin: 0;
+                    text-align: center;
+                    border: 2px solid #000000;
+                    border-bottom: 0;
+                }
+
+                .pwdreset form {
+                    border: 2px solid #000000;
+                    border-top: 0;
+                    background-color: #fff;
+
+                    -webkit-border-radius: 0 0 15px 15px;
+                    -moz-border-radius: 0 0 15px 15px;
+                    border-radius: 0 0 15px 15px;
+
+                }
+
+                .pwdreset fieldset {
+                    border: 0;
+                    margin: 0;
+                    padding: 20px 20px;
+                }
+
+                .pwdreset fieldset p {
+                    color: #222;
+                    margin: 0;
+                    margin-bottom: 8px;
+                }
+
+                .pwdreset fieldset p:last-child {
+                    margin-bottom: 0;
+                }
+
+                .pwdreset p.pwdreseterror {
+                    background-color: #F2DEDE;
+                    border-color: #EBCCD1;
+                    color: #A94442;
+                    padding: 10px;
+                    text-align: center;
+
+                    -webkit-border-radius: 3px;
+                    -moz-border-radius: 3px;
+                    border-radius: 3px;
+                }
+
+                input {
+                    border: 0;
+                    border-bottom: 1px solid #222;
+                    font-family: inherit;
+                    font-size: inherit;
+                    font-weight: inherit;
+                    line-height: inherit;
+                    -webkit-appearance: none;
+                }
+
+                .pwdreset fieldset input[type="text"], .pwdreset fieldset input[type="password"] {
+                    background-color: #e9e9e9;
+                    color: #222;
+                    padding: 4px;
+                    width: 256px;
+                    margin-bottom: 16px;
+                }
+
+                .pwdreset fieldset input[type="submit"] {
+                    background-color: #f7ce4a;
+                    color: #222;
+                    display: block;
+                    margin: 0 auto;
+                    padding: 4px;
+                    -webkit-border-radius: 3px;
+                    -moz-border-radius: 3px;
+                    border-radius: 3px;
+                    border: 0;
+                }
+
+                .pwdreset fieldset input[type="submit"]:hover {
+                    background-color: #deb531;
+                }
+            </style>
+        </head>
+        <body>
+        <div class="pwdreset">
+            <img src="<?php echo IMAGES_DIR . MW_LOGO; ?>" alt="<?php echo __('mwlogo99');?>">
+            <h1><?php echo __('title100'); ?></h1>
+            <?php if (file_exists('conf.php')) {
+                if ($fields != '') {
+                    ?>
+                    <form name="pwdresetform" class="pwdresetform" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <fieldset>
+                            <?php if (isset($_GET['error']) || $errors != '') {
+                                ?>
+                                <p class="pwdreseterror">
+                                    <?php echo $errors;?>
+                                </p>
+                                <?php
+                            } ?>
+                            <?php
+                            if ($fields == 'stage1') {
+                                ?>
+                                <p><label><?php echo __('emailaddress100');?></label></p>
+                                <p><input name="email" type="text" id="email" autofocus></p>
+                                <p><input type="submit" name="Submit" value="<?php echo __('requestpwdreset');?>"></p>
+                                <?php
+                            }
+                            if ($fields == 'stage2') {
+                                ?>
+                                <input type="hidden" name="email" value="<?php echo $email; ?>">
+                                <input type="hidden" name="uid" value="<?php echo $uid; ?>">
+                                <p><label><?php echo __('01pwd100');?></label></p>
+                                <p><input name="pwd1" type="password" id="pwd1" autofocus></p>
+                                <p><label><?php echo __('02pwd100');?></label></p>
+                                <p><input name="pwd2" type="password" id="pwd2"></p>
+                                <p><input type="submit" name="Submit" value="<?php echo __('button100');?>"></p>
+                                <?php
+                            }
+                            ?>
+
+                        </fieldset>
+                    </form>
+                    <?php
+                }
+                if($message!='')
+                {
+                    echo $message;
+                }
+
+            } else {
+                ?>
+                <p class="error">
+                    <?php echo __('cannot_read_conf'); ?>
+                </p>
+                <?php
+
+            }
+            ?>
+        </div>
+
+        </body>
+        </html>
+        <?php
+    }
+}
+else
+{
+    die(__('conferror100'));
+}
--- password-reset-sql.php
+++ password-reset-sql.php
@@ -5,5 +5,26 @@
  * Company: ASU Web Services LTD
  * Web: www.asuweb.co.uk
  * Date: 07/01/2017
- * Time: 10:16
+ * Time: 09:55
+ *
+ * Created for Mailwatch 1.2.0 RC4
  */
+header("Content-type: text/plain\n\n");
+require("/var/www/html/mailwatch-development/functions.php");
+
+$link = dbconn();
+echo "Testing DB Connection...";
+if($link) {
+    echo "OK...\n";
+    echo "Updating users table for password-reset...";
+    $sql = 'ALTER TABLE users ADD COLUMN (
+resetid varchar(255),
+resetexpire bigint(20),
+lastreset bigint(20)
+);';
+    $result = dbquery($sql);
+    if(!$result) echo "\nERROR Failed to modify table ".$link->error;
+    else echo "done\n";
+    echo "Finished\n";
+}
+else echo "DB Connection Failed..";
