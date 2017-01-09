cdb570231cf73ee4d830efe474dfeb798461d4e5
Manuel Dalla Lana <endelwar@aregar.it>
2017-01-09 12:24:45 +0100

    Give credit to all contributors of MailWatch

--- upgrade.php
+++ upgrade.php
@@ -5,7 +5,7 @@
  * MailWatch for MailScanner
  * Copyright (C) 2003-2011  Steve Freegard (steve@freegard.name)
  * Copyright (C) 2011  Garrod Alwood (garrod.alwood@lorodoes.com)
- * Copyright (C) 2014-2017  MailWatch Team (https://github.com/orgs/mailwatch/teams/team-stable)
+ * Copyright (C) 2014-2017  MailWatch Team (https://github.com/mailwatch/1.2.0/graphs/contributors)
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
