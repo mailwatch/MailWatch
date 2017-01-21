97dd838923795d98d07566c769f7569d4a9a92fc
Alan Urquhart <alan@asuweb.co.uk>
2017-01-21 13:56:50 +0000

    Fixed path to functions.php
    Fixed localisation issue

--- mailscanner/password_reset.php
+++ mailscanner/password_reset.php
@@ -257,7 +257,7 @@ if (defined('PWD_RESET') && PWD_RESET === true) {
         <head>
             <meta charset="utf-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <title>Password Reset Page</title>
+            <title><?php echo __('title100');?></title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="shortcut icon" href="images/favicon.png">
             <style type="text/css">
