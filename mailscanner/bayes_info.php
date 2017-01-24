860caf8839010dcb72ebdad9b573f84efb142696
stefaweb <sleclerc@actionweb.fr>
2017-01-24 16:30:30 +0100

    Update comment

--- mailscanner/bayes_info.php
+++ mailscanner/bayes_info.php
@@ -135,7 +135,7 @@ if ($_SESSION['user_type'] == 'A') {
             echo '<br>' . __('cannotfind18') . ' ' . SA_DIR . 'sa-learn';
             echo '</div>';
         } else {
-            // Using --force-expire instead of --clear for testing purpose
+            // Using --force-expire instead of --clear for testing purpose - TO REMOVE BEFORE PUBLISHING IT
             passthru(SA_DIR . 'sa-learn -p ' . SA_PREFS . ' --force-expire', $return);
             if ($return == 0) {
                 audit_log(__('auditlogwipe18'));
