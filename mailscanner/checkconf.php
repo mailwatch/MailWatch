bb99b2f1570d36fbaa1615489b55ef62f851abe8
Michael Pietsch <michael@mi-pietsch.de>
2017-01-25 10:30:37 +0100

    fix root path

--- mailscanner/checkconf.php
+++ mailscanner/checkconf.php
@@ -29,7 +29,7 @@
  * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-$mailscannerRoot = "/var/www/mailscanner/";
+$mailscannerRoot = "/var/www/html/mailscanner/";
 if (count($argv) > 1) {
     $mailscannerRoot = $argv[1];
 }
