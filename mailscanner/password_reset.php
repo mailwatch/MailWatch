0b1dbbaf58b7887af65adfcd651d669c2cbe3a2c
Alan Urquhart <alan@asuweb.co.uk>
2017-01-26 17:25:04 +0000

    Remove uneccessary tags

--- mailscanner/password_reset.php
+++ mailscanner/password_reset.php
@@ -375,20 +375,20 @@ if (defined('PWD_RESET') && PWD_RESET === true) {
             <div class="border-rounded">
                 <h1><?php echo __('title63'); ?></h1>
                 <?php if (file_exists('conf.php')) {
-            if ($fields !== '') {
-                ?>
+                    if ($fields !== '') {
+                        ?>
                         <form name="pwdresetform" class="pwdresetform" method="post"
                               action="<?php echo $_SERVER['PHP_SELF']; ?>">
                             <fieldset>
                                 <?php if (isset($_GET['error']) || $errors !== '') {
-                    ?>
+                                    ?>
                                     <p class="pwdreseterror">
                                         <?php echo $errors; ?>
                                     </p>
                                     <?php
 
-                } ?>
-                                <?php
+                                }
+
                                 if ($fields === 'stage1') {
                                     ?>
                                     <p><label><?php echo __('emailaddress63'); ?></label></p>
@@ -398,8 +398,8 @@ if (defined('PWD_RESET') && PWD_RESET === true) {
                                     <?php
 
                                 }
-                if ($fields === 'stage2') {
-                    ?>
+                                if ($fields === 'stage2') {
+                                    ?>
                                     <input type="hidden" name="email" value="<?php echo $email; ?>">
                                     <input type="hidden" name="uid" value="<?php echo $uid; ?>">
                                     <p><label><?php echo __('01pwd63'); ?></label></p>
@@ -409,25 +409,25 @@ if (defined('PWD_RESET') && PWD_RESET === true) {
                                     <p><input type="submit" name="Submit" value="<?php echo __('button63'); ?>"></p>
                                     <?php
 
-                } ?>
+                                } ?>
 
                             </fieldset>
                         </form>
                         <?php
 
-            } elseif ($message !== '') {
-                echo $message;
-            } elseif ($errors !== '') {
-                echo $errors;
-            }
-        } else {
-            ?>
+                    } elseif ($message !== '') {
+                        echo $message;
+                    } elseif ($errors !== '') {
+                        echo $errors;
+                    }
+                } else {
+                    ?>
                     <p class="error">
                         <?php echo __('cannot_read_conf'); ?>
                     </p>
                     <?php
 
-        } ?>
+                } ?>
             </div>
         </div>
 
