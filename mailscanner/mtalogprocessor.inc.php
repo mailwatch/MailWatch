5bd3fc976261696acacbedfc0924ee118d7a8752
Skywalker-11 <michael@mi-pietsch.de>
2017-01-27 17:15:33 +0100

    moved mtalogprocessor class to seperate file and move getIp + getEmail inside the class

---
+++ mailscanner/mtalogprocessor.inc.php
@@ -0,0 +1,210 @@
+<?php 
+
+/*
+ * MailWatch for MailScanner
+ * Copyright (C) 2003-2011  Steve Freegard (steve@freegard.name)
+ * Copyright (C) 2011  Garrod Alwood (garrod.alwood@lorodoes.com)
+ * Copyright (C) 2014-2017  MailWatch Team (https://github.com/mailwatch/1.2.0/graphs/contributors)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * In addition, as a special exception, the copyright holder gives permission to link the code of this program with
+ * those files in the PEAR library that are licensed under the PHP License (or with modified versions of those files
+ * that use the same license as those files), and distribute linked combinations including the two.
+ * You must obey the GNU General Public License in all respects for all of the code used other than those files in the
+ * PEAR library that are licensed under the PHP License. If you modify this program, you may extend this exception to
+ * your version of the program, but you are not obligated to do so.
+ * If you do not wish to do so, delete this exception statement from your version.
+ *
+ * As a special exception, you have permission to link this program with the JpGraph library and distribute executables,
+ * as long as you follow the requirements of the GNU GPL in regard to all of the software in the executable aside from
+ * JpGraph.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+class MtaLogProcessor {
+    $mtaprocess;
+    $delayField;
+    $statusField;
+    
+    $raw;
+    $id;
+    $entry;
+    $entries;
+
+    abstract function extractKeyValuePairs($match);
+    
+    function getRejectReasons() {
+        return array();
+    }
+    
+    function getRulesets() {
+        return array();
+    }
+    
+    function doit($input)
+    {
+        global $fp;//TODO do we need this?
+        if (!$fp = popen($input, 'r')) {
+            die(__('diepipe56')); //TODO remember to change the mapping of ...56 to this file
+        }
+        dbconn();
+
+        $lines = 1;
+        while ($line = fgets($fp, 2096)) {        
+            // Reset variables
+            unset($parsed, $mta_parser, $_timestamp, $_host, $_type, $_msg_id, $_status);
+            
+            $parsed = new SyslogParser($line);
+            $_timestamp = safe_value($parsed->timestamp);
+            $_host = safe_value($parsed->host);
+            $_dsn = '';
+            $_delay = '';
+            $_relay = '';
+
+            if ($parsed->process === $this->mtaprocess) {
+                $this->parse($parsed->entry);
+                if (true === DEBUG) {
+                    print_r($this);
+                }
+
+                $_msg_id = safe_value($this->id);
+                
+                //apply rulesets if they exist
+                $rulesets = getRulesets();
+                if(isset($rulesets['type'])) {
+                    $_type = $rulesets['type'];
+                }
+                if(isset($rulesets['relay'])) {
+                    $_relay = $rulesets['relay'];
+                }
+                if(isset($rulesets['status'])) {
+                    $_status = $rulesets['status'];
+                }
+
+                // Milter-ahead rejections
+                if (preg_match('/Milter: /i', $this->raw) && preg_match(
+                        '/(rejected recipient|user unknown)/i',
+                        $this->entries['reject']
+                    )
+                ) {
+                    $_type = safe_value('unknown_user');
+                    $_status = safe_value($this->getEmail());
+                }
+
+                // Unknown users
+                if (preg_match('/user unknown/i', $this->entry)) {
+                    // Unknown users
+                    $_type = safe_value('unknown_user');
+                    $_status = safe_value($this->raw);
+                }
+                
+                //apply reject reasons if they exist
+                $rejectReasons = getRejectReasons();
+                if(isset($rejectReasons['type'])) {
+                    $_type = $rejectReasons['type'];
+                }
+                if(isset($rejectReasons['status'])) {
+                    $_status = $rejectReasons['status'];
+                }
+
+                // Relay lines
+                if (isset($this->entries['relay'], $this->entries[$statusField])) {
+                    $_type = safe_value('relay');
+                    $_delay = safe_value($this->entries[$this->delayField]);
+                    $_relay = safe_value($this->getIp());
+                    $_dsn = safe_value($this->entries['dsn']);
+                    $_status = safe_value($this->entries[$this->statusField]);
+                }
+            }
+            if (isset($_type)) {
+                dbquery(
+                    "REPLACE INTO mtalog VALUES (FROM_UNIXTIME('$_timestamp'),'$_host','$_type','$_msg_id','$_relay','$_dsn','$_status','$_delay')"
+                );
+            }
+            $lines++;
+        }
+        dbclose();
+        pclose($fp);
+    }
+
+    /**
+     * @param string $line
+     */
+    public function parse($line)
+    {
+        //reset the variables
+        $this->id = null;
+        $this->entry = null;
+        $this->entries = null;
+        $this->raw = $line;
+        
+        //do the parse
+        if (preg_match('/^(\S+):\s(.+)$/', $line, $match)) {
+            $this->id = $match[1];
+
+            // Milter
+            if (preg_match('/(\S+):\sMilter:\s(.+)$/', $line, $milter)) {
+                $match = $milter;
+            }
+
+            // Extract any key=value pairs
+            if (strstr($match[2], '=')) {
+                //calls the function passed as argument
+                $this->entries = $this->extractKeyValuePairs($match);
+            } else {
+                $this->entry = $match[2];
+            }
+        } else {
+            // No message ID found
+            // Extract any key=value pairs
+            if (strstr($this->raw, '=')) {
+                $items = explode(', ', $this->raw);
+                $entries = array();
+                foreach ($items as $item) {
+                    $entry = explode('=', $item);
+                    // fix for the id= issue 09.12.2011
+                    if (isset($entry[2])) {
+                        $entries[$entry[0]] = $entry[1] . '=' . $entry[2];
+                    } else {
+                        $entries[$entry[0]] = $entry[1];
+                    }
+                }
+                $this->entries = $entries;
+            } else {
+                return false;
+            }
+        }
+    } 
+    
+    /**
+     * @return string
+     */
+    function getIp()
+    {
+        if (preg_match('/\[(\d+\.\d+\.\d+\.\d+)\]/', $this->entries['relay'], $match)) {
+            return $match[1];
+        } else {
+            return $this->entries['relay'];
+        }
+    }
+
+    /**
+     * @return string
+     */
+    function getEmail()
+    {
+        if (preg_match('/<(\S+)>/', $this->entries['to'], $match)) {
+            return $match[1];
+        } else {
+            return $this->entries['to'];
+        }
+    }
+}
