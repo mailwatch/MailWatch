9dcb3ac6fca7295d8c6854104571d89ac5413d1b
stefaweb <sleclerc@actionweb.fr>
2017-02-06 20:31:47 +0100

    Forgot one InnoDB

--- upgrade.php
+++ upgrade.php
@@ -401,7 +401,7 @@ if ($link) {
             echo ' DO NOT EXISTS' . PHP_EOL;
         } else {
             if (check_utf8_table(DB_NAME, $table, $server_utf8_variant) === false) {
-                $sql = 'ALTER TABLE `' . $table . '` ENGINE = INNODB';
+                $sql = 'ALTER TABLE `' . $table . '` ENGINE = InnoDB';
                 executeQuery($sql);
             } else {
                 echo ' ALREADY CONVERTED' . PHP_EOL;
