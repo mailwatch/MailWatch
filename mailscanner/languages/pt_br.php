1f28dbdc641c84ff543b0aeca5c0301a36772fc1
stefaweb <sleclerc@actionweb.fr>
2017-02-06 13:43:26 +0100

    Update of unknownvirusscanner03

--- mailscanner/languages/pt_br.php
+++ mailscanner/languages/pt_br.php
@@ -185,7 +185,7 @@ return array(
     '15minutes03' => '15 minutes:',
     'saspam03' => 'spam',
     'sanotspam03' => 'not spam',
-    'unknownvirusscanner03' => 'Unknown virus scanner',
+    'unknownvirusscanner03' => 'Unknown virus scanner defined in MailScanner.conf. Review your configuration and don\'t use \'auto\' as antivirus name (see FAQ).',
 
     // 04-detail.php
     'receivedon04' => 'Recebido em:',
