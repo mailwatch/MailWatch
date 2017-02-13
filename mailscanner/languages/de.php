6bd15060414a04fcb135de80734610b59223dc50
asuweb <alan@asuweb.co.uk>
2017-02-13 18:05:01 +0000

    Autorelease - fixed missing html (#503)

--- mailscanner/languages/de.php
+++ mailscanner/languages/de.php
@@ -796,13 +796,14 @@ return array(
     // 59-auto-release.php
     'msgnotfound159' => 'Nachricht nicht gefunden. Möglicherweise haben Sie die Nachricht bereits freigegeben oder der Link ist abgelaufen.',
     'msgnotfound259' => 'Bitte kontaktieren Sie Ihren Mail-Administrator und teilen Sie ihm diese Nachrichten-ID mit: ',
-    'msgnotfound393' => 'wenn diese Nachricht freigegeben werden soll.',
+    'msgnotfound359' => 'wenn diese Nachricht freigegeben werden soll.',
     'msgreleased59' => 'Nachricht freigegeben<br>Möglicherweise dauert es wenige Minuten, bis diese in Ihrem Posteingang eintrifft.',
     'tokenmismatch59' => 'Fehler beim freigeben der Nachricht - Token stimmen nicht überein',
     'notallowed59' => 'Sie haben keine Berechtigung zum Zugriff auf diese Seite!',
     'dberror59' => 'Etwas ist schief gegangen - Kontaktieren Sie den Administrator',
     'arview059' => 'Anzeigen',
     'arrelease59' => 'Freigeben',
+    'title59' => 'Quarantine Release',
 
      // 60-rpcserver.php
     'paratype160' => 'Parameter-Typ',
--- mailscanner/languages/en.php
+++ mailscanner/languages/en.php
@@ -795,14 +795,15 @@ return array(
 
     // 59-auto-release.php
     'msgnotfound159' => 'Message not found.  You may have already released this message or the link may have expired.',
-    'msgnotfound259' => 'Please contact your Mail Administrator and provide him with this message ID: ',
-    'msgnotfound393' => 'if you need this message released',
+    'msgnotfound259' => 'Please contact your Mail Administrator and provide them with this message ID: ',
+    'msgnotfound359' => 'if you need this message released',
     'msgreleased59' => 'Message released<br>It may take a few minutes to appear in your Inbox.',
     'tokenmismatch59' => 'Error releasing message - token mismatch',
     'notallowed59' => 'You are not allowed to be here!',
     'dberror59' => 'Something went wrong - please contact support',
     'arview059' => 'View',
     'arrelease59' => 'Release',
+    'title59' => 'Quarantine Release',
 
     // 60-rpcserver.php
     'paratype160' => 'Parameter type',
