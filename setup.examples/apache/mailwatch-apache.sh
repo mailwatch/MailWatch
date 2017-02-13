d7515b4f985e1331874b72cafa70d55925e60abe
Michael Pietsch <michael@mi-pietsch.de>
2017-02-13 12:50:05 +0100

    fix apache configuration

--- setup.examples/apache/mailwatch-apache.sh
+++ setup.examples/apache/mailwatch-apache.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WebFolder="$1"
-
+echo $WebFolder
 if ( type "apache2" > /dev/null 2>&1 ); then
   apacheBin="apache2"
 else
   apacheBin="httpd"
 fi
-if [ -z $("$apacheBin" -v | grep "Apache/2.4") ]
-    sed -i -e "s/ALLGRANTED/Require all granted/" "$DIR/etc/apache2/conf-available/mailwatch.conf"
-  else
-    sed -i -e "s/ALLGRANTED/Order allow,deny\n  Allow from all/" "$DIR/etc/apache2/conf-available/mailwatch.conf"
-fi
-# a2enconf mailwatch.conf
-ln -r -s /etc/apache2/conf-enabled/mailwatch.conf /etc/apache2/conf-available/mailwatch.conf
 
-sed -i -e "s/WEBFOLDER/$WebFolder/" "$DIR/etc/apache2/conf-enabled/mailwatch.conf"
-cp "$DIR/etc/apache2/conf-enabled/mailwatch.conf" /etc/apache2/conf-enabled/mailwatch.conf
+echo $apache24
+if [[ -z $apache24 ]]; then
+    sed -i -e "/ALLGRANTED/Require all granted/" "$DIR/etc/apache2/conf-available/mailwatch.conf"
+else
+    sed -i -e "/ALLGRANTED/Order allow,deny\n  Allow from all/" "$DIR/etc/apache2/conf-available/mailwatch.conf"
+fi
+sed -i -e "s~WEBFOLDER~$WebFolder~" "$DIR/etc/apache2/conf-available/mailwatch.conf"
+
+#backup old config
+if [ -f /etc/apache2/conf-available/mailwatch.conf ]; then
+    mv /etc/apache2/conf-available/mailwatch.conf /etc/apache2/conf-available/mailwatch.conf.old
+fi
+
+cp "$DIR/etc/apache2/conf-available/mailwatch.conf" /etc/apache2/conf-available/mailwatch.conf
+
+# a2enconf mailwatch.conf
+ln -f -r -s /etc/apache2/conf-enabled/mailwatch.conf /etc/apache2/conf-available/mailwatch.conf
 
 a2enmod ssl
 service apache2 reload
