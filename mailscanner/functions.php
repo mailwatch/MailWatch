9bbf30e8fcd4d721bd3bceed13478e711ead57f0
stefaweb <sleclerc@actionweb.fr>
2017-02-21 08:56:39 +0100

    Update

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -2122,12 +2122,10 @@ function db_colorised_table($sql, $table_heading = false, $pager = false, $order
                     case 'report':
                         // IMPORTANT NOTE: for this to work correctly the 'report' field MUST
                         // appear after the 'virusinfected' field within the SQL statement.
-                        if (defined('VIRUS_REGEX') && preg_match(VIRUS_REGEX, $row[$f], $virus)) {
-                            if (DISPLAY_VIRUS === true) {
-                                foreach ($status_array as $k => $v) {
-                                    if ($v = str_replace('Virus', 'Virus (' . return_virus_link($virus[2]) . ')', $v)) {
-                                        $status_array[$k] = $v;
-                                    }
+                        if (defined('VIRUS_REGEX') && preg_match(VIRUS_REGEX, $row[$f], $virus) && DISPLAY_VIRUS === true) {
+                            foreach ($status_array as $k => $v) {
+                                if ($v = str_replace('Virus', 'Virus (' . return_virus_link($virus[2]) . ')', $v)) {
+                                    $status_array[$k] = $v;
                                 }
                             }
                         }
