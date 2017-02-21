b03310829a6131c88ac4641ae305db320f3a8b72
stefaweb <sleclerc@actionweb.fr>
2017-02-21 08:42:48 +0100

    DISPLAY_VIRUS option on message listing

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -2123,11 +2123,13 @@ function db_colorised_table($sql, $table_heading = false, $pager = false, $order
                         // IMPORTANT NOTE: for this to work correctly the 'report' field MUST
                         // appear after the 'virusinfected' field within the SQL statement.
                         if (defined('VIRUS_REGEX') && preg_match(VIRUS_REGEX, $row[$f], $virus)) {
-                            foreach ($status_array as $k => $v) {
-                                if ($v = str_replace('Virus', 'Virus (' . return_virus_link($virus[2]) . ')', $v)) {
-                                    $status_array[$k] = $v;
-                                }
-                            }
+                        	if (DISPLAY_VIRUS === true) {
+	                            foreach ($status_array as $k => $v) {
+	                                if ($v = str_replace('Virus', 'Virus (' . return_virus_link($virus[2]) . ')', $v)) {
+	                                    $status_array[$k] = $v;
+	                                }
+	                            }
+	                        }
                         }
                         break;
                     case 'nameinfected':
@@ -4011,6 +4013,7 @@ function checkConfVariables()
         'USE_LDAP',
         'USE_PROXY',
         'VIRUS_INFO',
+        'DISPLAY_VIRUS',
     );
 
     $obsolete = array(
