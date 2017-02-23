37bdf81b8963f2832cd0d9cc3d84a23f01f67ce3
Alan Urquhart <alan@asuweb.co.uk>
2017-02-23 11:46:10 +0000

    Modified to make user editing easier

--- MailScanner_perl_scripts/MailWatch-DB.pm
+++ MailScanner_perl_scripts/MailWatch-DB.pm
@@ -1,6 +1,18 @@
 #!/usr/bin/perl
-sub mailwatch_get_db_name { return 'mailscanner'};
-sub mailwatch_get_db_host { return 'localhost' };
-sub mailwatch_get_db_user { return 'mailwatch' };
-sub mailwatch_get_db_password { return 'mailwatch' };
+
+# Change the values below to match the MailWatch database settings
+# as set in conf.php
+my ($db_name) = 'mailscanner';
+my ($db_host) = 'localhost';
+my ($db_user) = 'mailwatch';
+my ($db_pass) = 'mailwatch';
+
+###############################
+# don't touch below this line #
+###############################
+
+sub get_db_name { return $db_name };
+sub get_db_host { return $db_host };
+sub get_db_user { return $db_user };
+sub get_db_password { return $db_pass };
 1;
