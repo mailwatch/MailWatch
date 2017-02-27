f16b40e56cdbb849b3819c1491925e3a7b0b1172
Stéphane <sleclerc@actionweb.fr>
2017-02-27 10:31:18 +0100

    Fix for $refresh_time to use MailWatch-DB.pm (#589) (#594)

--- MailScanner_perl_scripts/MailWatch-DB.pm
+++ MailScanner_perl_scripts/MailWatch-DB.pm
@@ -7,6 +7,9 @@ my ($db_host) = 'localhost';
 my ($db_user) = 'mailwatch';
 my ($db_pass) = 'mailwatch';
 
+# Change the value below for SQLBlackWhiteList.pm (default = 15)
+my ($bwl_refresh_time) = 15;      # Time in minutes before lists are refreshed
+
 ###############################
 # don't touch below this line #
 ###############################
@@ -15,4 +18,5 @@ sub mailwatch_get_db_name { return $db_name };
 sub mailwatch_get_db_host { return $db_host };
 sub mailwatch_get_db_user { return $db_user };
 sub mailwatch_get_db_password { return $db_pass };
+sub mailwatch_get_BWL_refresh_time { return $bwl_refresh_time };
 1;
