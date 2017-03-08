33f76f52a803ae442a9a5b563eb131a953b8e731
stefaweb <sleclerc@actionweb.fr>
2017-03-08 16:16:23 +0100

    Change name for 00MailWatchConf
    Fix bug with MailScanner prior to 5.0.4.

--- MailScanner_perl_scripts/MailWatch.pm
+++ MailScanner_perl_scripts/MailWatch.pm
@@ -61,10 +61,10 @@ my $server_port = 11553;
 my $timeout = 3600;
 my ($SQLversion);
 
-# Get database information from 00-MailWatch-conf.pm
+# Get database information from 00MailWatchConf.pm
 use File::Basename;
 my $dirname = dirname(__FILE__);
-require $dirname.'/00-MailWatch-conf.pm';
+require $dirname.'/00MailWatchConf.pm';
 
 my ($db_name) = mailwatch_get_db_name();
 my ($db_host) = mailwatch_get_db_host();
