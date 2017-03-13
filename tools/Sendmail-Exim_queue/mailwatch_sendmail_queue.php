1a745f00dbbfe6fa798d8899246b520c27b5223f
stefaweb <sleclerc@actionweb.fr>
2017-03-13 12:04:43 +0100

    Update

--- tools/Sendmail-Exim_queue/mailwatch_sendmail_queue.php
+++ tools/Sendmail-Exim_queue/mailwatch_sendmail_queue.php
@@ -273,9 +273,9 @@ if (false !== $fl && flock($fl, LOCK_EX + LOCK_NB)) {
     ('" . safe_value($msgid) . "','" .
                     safe_value($msginfo['cdate']) . "','" .
                     safe_value($msginfo['ctime']) . "','" .
-                    safe_value($msginfo['sender']) . "','" .
+                    safe_value(isset($msginfo['sender']) ? $msginfo['sender'] : "") . "','" .
                     safe_value(@implode(',', $msginfo['rcpts'])) . "','" .
-                    safe_value($msginfo['subject']) . "','" .
+                    safe_value(isset($msginfo['subject']) ? $msginfo['subject'] : "") . "','" .
                     safe_value($msginfo['message']) . "','" .
                     safe_value($msginfo['size']) . "','" .
                     safe_value($msginfo['priority']) . "','" .
