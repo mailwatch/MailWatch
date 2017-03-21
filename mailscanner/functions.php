51d04f1696e38a8c50f60dd8a0e34bd30750c5f9
stefaweb <sleclerc@actionweb.fr>
2017-03-21 08:20:33 +0100

    Typo

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -472,10 +472,10 @@ function html_start($title, $refresh = 0, $cacheable = true, $report = false)
                     $outq = exec('sudo ' . EXIM_QUEUE_OUT . ' 2>&1');
                 } else {
                     $cmd = exec('sudo /usr/sbin/sendmail -bp -OQueueDirectory=/var/spool/mqueue.in 2>&1');
-                    preg_match"/(Total requests: )(.*)/", $cmd, $output_array);
+                    preg_match("/(Total requests: )(.*)/", $cmd, $output_array);
                     $inq = $output_array[2];
                     $cmd = exec('sudo /usr/sbin/sendmail -bp -OQueueDirectory=/var/spool/mqueue.in 2>&1');
-                    preg_match"/(Total requests: )(.*)/", $cmd, $output_array);
+                    preg_match("/(Total requests: )(.*)/", $cmd, $output_array);
                     $outq = $output_array[2];
                     //$inq = database::mysqli_result(dbquery('SELECT COUNT(*) FROM inq WHERE ' . $_SESSION['global_filter']),
                     //    0);
