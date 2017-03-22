7338f38f5d98b453293d9a7ee7502b43ad9f3f46
stefaweb <sleclerc@actionweb.fr>
2017-03-22 08:15:03 +0100

    fr.php update

--- mailscanner/languages/fr.php
+++ mailscanner/languages/fr.php
@@ -465,7 +465,7 @@ return array(
     'retypepassword12' => 'Confirmez le mot de passe :',
     'userexists12' => 'L\'utilisateur existe déjà avec le nom d\'utilisateur %s',
     'savedsettings12' => 'Règlages enregistrés',
-    'errordeleteself12' => 'Error: You can\'t delete yourself!',
+    'errordeleteself12' => 'Erreur : vous ne pouvez pas supprimer votre propre compte !',
 
     // 13-sa_rules_update.php
     'input13' => 'Démarrer',
