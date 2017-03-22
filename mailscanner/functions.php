7bc2976e99131f58eae9746407ba457df7219b2d
Manuel Dalla Lana <endelwar@aregar.it>
2017-03-22 18:15:38 +0100

    Specify when MAILQ config entry is needed

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -4143,7 +4143,7 @@ function checkConfVariables()
         'EXIM_QUEUE_OUT' => array('description' => 'needed only if using Exim as MTA'),
         'PWD_RESET_FROM_NAME' => array('description' => 'needed if Password Reset feature is enabled'),
         'PWD_RESET_FROM_ADDRESS' => array('description' => 'needed if Password Reset feature is enabled'),
-        'MAILQ' => array('description' => 'needed to display the inbound/outbound mail queue lengths'),
+        'MAILQ' => array('description' => 'needed when using Exim or Sendmail to display the inbound/outbound mail queue lengths'),
         'MAIL_SENDER'  => array('description' => 'needed if you use Exim or Sendmail Queue'),
     );
 
