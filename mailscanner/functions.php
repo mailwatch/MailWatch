ae106ef1c5deab16a98b47e98533a9c9879f9286
Michael Pietsch <michael@mi-pietsch.de>
2017-03-22 17:55:24 +0100

    add percentage values to legend and graph

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -4043,6 +4043,32 @@ function printGraphTable($sqlDataQuery, $reportTitle, $sqlColumns, $columns, $gr
     echo '<canvas id="reportChart" class="reportGraph"></canvas>
   <script src="lib/Chart.js/Chart.min.js"></script>
   <script>
+    function drawPersistentPercentValues() {
+      var ctx = this.chart.ctx;
+      ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, "normal", Chart.defaults.global.defaultFontFamily);
+      ctx.fillStyle = this.chart.config.options.defaultFontColor;
+      this.data.datasets.forEach(function (dataset) {
+        var sum =0;
+        for (var i = 0; i < dataset.data.length; i++) {
+          if(dataset.hidden === true || dataset._meta[0].data[i].hidden === true){ continue; }
+          sum += dataset.data[i];
+        }
+        var curr= 0;
+        for (var i = 0; i < dataset.data.length; i++) {
+          if(dataset.hidden === true || dataset._meta[0].data[i].hidden === true){ continue; }
+          var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model;
+          if(dataset.data[i] !== null) {
+            var part = dataset.data[i]/sum;
+            var radius = model.outerRadius-10; //where to place the text around the center
+            var x = Math.sin((curr+part/2)*2*Math.PI)*radius;
+            var y = Math.cos((curr+part/2)*2*Math.PI)*radius;
+            ctx.fillText((part*100).toFixed(0)+"%",model.x + x - 8 , model.y - y - 5 );
+            curr += part;
+          }
+        }
+      });
+    }
+
     var ctx = document.getElementById("reportChart");
     var myChart = new Chart(ctx, {
       type: "pie",
@@ -4061,7 +4087,7 @@ function printGraphTable($sqlDataQuery, $reportTitle, $sqlColumns, $columns, $gr
         },
         legend: {
           display: true,
-          position: "top"
+          position: "right",
         },
         responsive: false,
         tooltips: {
@@ -4078,7 +4104,31 @@ function printGraphTable($sqlDataQuery, $reportTitle, $sqlColumns, $columns, $gr
               return tooltipLabel + ": " + tooltipData + " (" + tooltipPercentage + "%)";
             }
           }
-        }
+        },
+        legend: {
+          labels: {
+            generateLabels: function(graph) {
+              var graphData = graph.data.datasets[0].data;
+              var total = 0;
+              for(var i=0; i<graphData.length; i++) {
+                total += graphData[i];
+              };
+              var defaultLabels = Chart.defaults.doughnut.legend.labels.generateLabels(graph);
+                console.log(defaultLabels);
+              for(var i=0; i<defaultLabels.length; i++) {
+                var label = defaultLabels[i];
+                var percentage = Math.round((graphData[i] / total) * 100);
+                defaultLabels[i].text += " (" + percentage +"%)";
+              }
+              return defaultLabels;
+            }
+          }
+        },
+        animation: {
+          onProgress: drawPersistentPercentValues,
+          onComplete: drawPersistentPercentValues
+        },
+        hover: { animationDuration: 0 }
       }
     });
   </script>';
