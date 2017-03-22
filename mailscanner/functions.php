c3251df23171dbd8ceba28244265bb70d6eed4d3
Michael Pietsch <michael@mi-pietsch.de>
2017-03-22 13:05:18 +0100

    add percentage to tooltip

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -4063,7 +4063,22 @@ function printGraphTable($sqlDataQuery, $reportTitle, $sqlColumns, $columns, $gr
           display: true,
           position: "top"
         },
-        responsive: false
+        responsive: false,
+        tooltips: {
+          callbacks: {
+            label: function(tooltipItem, data) {
+              var allData = data.datasets[tooltipItem.datasetIndex].data;
+              var tooltipLabel = data.labels[tooltipItem.index];
+              var tooltipData = allData[tooltipItem.index];
+              var total = 0;
+              for (var i in allData) {
+                total += allData[i];
+              }
+              var tooltipPercentage = Math.round((tooltipData / total) * 100);
+              return tooltipLabel + ": " + tooltipData + " (" + tooltipPercentage + "%)";
+            }
+          }
+        }
       }
     });
   </script>';
