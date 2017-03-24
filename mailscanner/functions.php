b471a6b46359ce7c38d3d1daa16ec17f60147f1a
stefaweb <sleclerc@actionweb.fr>
2017-03-24 08:31:10 +0100

    Update

--- mailscanner/functions.php
+++ mailscanner/functions.php
@@ -474,13 +474,9 @@ function html_start($title, $refresh = 0, $cacheable = true, $report = false)
                     $cmd = exec('sudo /usr/sbin/sendmail -bp -OQueueDirectory=/var/spool/mqueue.in 2>&1');
                     preg_match("/(Total requests: )(.*)/", $cmd, $output_array);
                     $inq = $output_array[2];
-                    $cmd = exec('sudo /usr/sbin/sendmail -bp -OQueueDirectory=/var/spool/mqueue.in 2>&1');
+                    $cmd = exec('sudo /usr/sbin/sendmail -bp 2>&1');
                     preg_match("/(Total requests: )(.*)/", $cmd, $output_array);
                     $outq = $output_array[2];
-                    //$inq = database::mysqli_result(dbquery('SELECT COUNT(*) FROM inq WHERE ' . $_SESSION['global_filter']),
-                    //    0);
-                    //$outq = database::mysqli_result(dbquery('SELECT COUNT(*) FROM outq WHERE ' . $_SESSION['global_filter']),
-                    //    0);
                 }
                 echo '    <tr><td colspan="3" class="heading" align="center">' . __('mailqueue03') . '</td></tr>' . "\n";
                 echo '    <tr><td colspan="2"><a href="mailq.php?token=' . $_SESSION['token'] . '&amp;queue=inq">' . __('inbound03') . '</a></td><td align="right">' . $inq . '</td>' . "\n";
