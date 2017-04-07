a1a780be18e717bdbcf0e4b5f3017666aec397d4
stefaweb <sleclerc@actionweb.fr>
2017-04-07 17:12:09 +0200

    de.php

--- mailscanner/languages/de.php
+++ mailscanner/languages/de.php
@@ -816,7 +816,7 @@ return array(
     'fileclosed55' => 'Datei geschlossen.',
     'donewrite55' => 'Schreiben der Datei abgeschlossen.',
     'conditions55' => 'Bedingungen:',
-    'and55' => 'and',
+    'and55' => 'und',
 
     // 56-mtalogprocessor.inc.php
     'diepipe56' => 'Kann Pipe nicht öffnen',
