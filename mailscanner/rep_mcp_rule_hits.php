45969b0c8baf693a79cdb94190ea29f8f618fbf8
shawniverson <shawniverson@gmail.com>
2017-04-10 06:02:31 -0400

    Load Filter class before session_start in functions.php

--- mailscanner/rep_mcp_rule_hits.php
+++ mailscanner/rep_mcp_rule_hits.php
@@ -30,8 +30,8 @@
  */
 
 // Include of necessary functions
-require_once __DIR__ . '/functions.php';
 require_once __DIR__ . '/filter.inc.php';
+require_once __DIR__ . '/functions.php';
 
 // Authentication checking
 require __DIR__ . '/login.function.php';
