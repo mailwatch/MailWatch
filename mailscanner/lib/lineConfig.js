08486e7748583a31c1351fef225baa90b8ff4ad8
stefaweb <sleclerc@actionweb.fr>
2017-06-19 10:49:12 +0200

    Adjusted blue (same as menu)

--- mailscanner/lib/lineConfig.js
+++ mailscanner/lib/lineConfig.js
@@ -2,7 +2,7 @@
  * This customizes a line chart for ChartJs. This files requires that the variables chartTitle, chartId, chartFormattedData and chartNumericData, fillBelowLine, COLON are already set.
  */
 var lineColors= [
-  '#6195f3', // blue
+  '#4973f7', // blue
   '#B22222', // dark red
   '#EE6262', // red
   '#f5d932', // yellow
