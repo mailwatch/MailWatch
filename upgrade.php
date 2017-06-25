92a29a9ed8faa5696f35f03309b1f4b09bc841f3
Michael Pietsch <michael@mi-pietsch.de>
2017-06-25 22:58:58 +0200

    fix quote

--- upgrade.php
+++ upgrade.php
@@ -242,7 +242,7 @@ function getSqlServer()
 {
     global $link;
     //test if mysql or mariadb is used.
-    $sql = 'SELECT VERSION() as version"';
+    $sql = 'SELECT VERSION() as version';
     $result = $link->query($sql);
     if (strpos($result->fetch_array['version'], 'MariaDB') === false) {
         //mysql does not support aria storage engine
