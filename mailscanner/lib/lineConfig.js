a6e6ea6e52447ca2eda6972be2df24fe6ae005e3
stefaweb <sleclerc@actionweb.fr>
2017-06-28 17:36:21 +0200

    lineConfig.js update

--- mailscanner/lib/lineConfig.js
+++ mailscanner/lib/lineConfig.js
@@ -18,6 +18,96 @@ var defaultColors= [
   mcpColor
 ];
 
+// See https://stackoverflow.com/questions/37250456/chart-js-evenly-distribute-ticks-when-using-maxtickslimit/37257056#37257056
+Chart.pluginService.register({
+  beforeRender: function(chart) {
+    var xScale = chart.scales['x-axis-0'];
+    if (xScale.options.ticks.maxTicksLimit) {
+      // store the original maxTicksLimit
+      xScale.options.ticks._maxTicksLimit = xScale.options.ticks.maxTicksLimit;
+      // let chart.js draw the first and last label
+      xScale.options.ticks.maxTicksLimit = (xScale.ticks.length % xScale.options.ticks._maxTicksLimit === 0) ? 1 : 2;
+    }
+  },
+
+  afterDraw: function(chart, easing) {
+    var xScale = chart.scales['x-axis-0'];
+    if (xScale.options.ticks.maxTicksLimit) {
+      var helpers = Chart.helpers;
+
+      var tickFontColor = helpers.getValueOrDefault(xScale.options.ticks.fontColor, Chart.defaults.global.defaultFontColor);
+      var tickFontSize = helpers.getValueOrDefault(xScale.options.ticks.fontSize, Chart.defaults.global.defaultFontSize);
+      var tickFontStyle = helpers.getValueOrDefault(xScale.options.ticks.fontStyle, Chart.defaults.global.defaultFontStyle);
+      var tickFontFamily = helpers.getValueOrDefault(xScale.options.ticks.fontFamily, Chart.defaults.global.defaultFontFamily);
+      var tickLabelFont = helpers.fontString(tickFontSize, tickFontStyle, tickFontFamily);
+      var tl = xScale.options.gridLines.tickMarkLength;
+
+      var isRotated = xScale.labelRotation !== 0;
+      var yTickStart = xScale.top;
+      var yTickEnd = xScale.top + tl;
+      var chartArea = chart.chartArea;
+
+      // use the saved ticks
+      var maxTicks = xScale.options.ticks._maxTicksLimit - 1;
+      var ticksPerVisibleTick = xScale.ticks.length / maxTicks;
+
+      // chart.js uses an integral skipRatio - this causes all the fractional ticks to be accounted for between the last 2 labels
+      // we use a fractional skipRatio
+      var ticksCovered = 0;
+      helpers.each(xScale.ticks, function(label, index) {
+        if (index < ticksCovered)
+          return;
+
+        ticksCovered += ticksPerVisibleTick;
+
+        // chart.js has already drawn these 2
+        if (index === 0 || index === (xScale.ticks.length - 1))
+          return;
+
+        // copy of chart.js code
+        var xLineValue = this.getPixelForTick(index);
+        var xLabelValue = this.getPixelForTick(index, this.options.gridLines.offsetGridLines);
+
+        if (this.options.gridLines.display) {
+          this.ctx.lineWidth = this.options.gridLines.lineWidth;
+          this.ctx.strokeStyle = this.options.gridLines.color;
+
+          xLineValue += helpers.aliasPixel(this.ctx.lineWidth);
+
+          // Draw the label area
+          this.ctx.beginPath();
+
+          if (this.options.gridLines.drawTicks) {
+            this.ctx.moveTo(xLineValue, yTickStart);
+            this.ctx.lineTo(xLineValue, yTickEnd);
+          }
+
+          // Draw the chart area
+          if (this.options.gridLines.drawOnChartArea) {
+            this.ctx.moveTo(xLineValue, chartArea.top);
+            this.ctx.lineTo(xLineValue, chartArea.bottom);
+          }
+
+          // Need to stroke in the loop because we are potentially changing line widths & colours
+          this.ctx.stroke();
+        }
+
+        if (this.options.ticks.display) {
+          this.ctx.save();
+          this.ctx.translate(xLabelValue + this.options.ticks.labelOffset, (isRotated) ? this.top + 12 : this.options.position === "top" ? this.bottom - tl : this.top + tl);
+          this.ctx.rotate(helpers.toRadians(this.labelRotation) * -1);
+          this.ctx.font = tickLabelFont;
+          this.ctx.fillStyle = tickFontColor;
+          this.ctx.textAlign = (isRotated) ? "right" : "center";
+          this.ctx.textBaseline = (isRotated) ? "middle" : this.options.position === "top" ? "bottom" : "top";
+          this.ctx.fillText(label, 0, 0);
+          this.ctx.restore();
+        }
+      }, xScale);
+    }
+  }
+});
+
 function getColor(axisid, lineid, datasetid, customColors) {
   if (typeof customColors !== 'undefined') {
     var color = customColors[axisid][lineid];
@@ -67,6 +157,8 @@ function printLineGraph(chartId, settings) {
       },
       legend: {
         display: (typeof settings.chartDataLabels === 'undefined' ? false : true),
+        // For Padding look at https://stackoverflow.com/questions/42585861/chart-js-increase-spacing-between-legend-and-chart
+        // or https://stackoverflow.com/questions/42870869/chartjs-top-and-bottom-padding-of-a-chart-area
       },
       elements: {
         line: {
@@ -95,7 +187,7 @@ function printLineGraph(chartId, settings) {
               ticks: {
                 suggestedMax: max * 1.05,
                 min: 0,
-                callback: ((typeof settings.valueTypes === 'undefined' || settings.valueTypes[i] == 'plain') ? 
+                callback: ((typeof settings.valueTypes === 'undefined' || settings.valueTypes[i] == 'plain') ?
                             Chart.Ticks.formatters.linear :
                             formatBytes
                 )
@@ -106,7 +198,7 @@ function printLineGraph(chartId, settings) {
         })(),
         xAxes: [{
           maxBarThickness: 7,
-        gridLines: {offsetGridLines: false},
+          gridLines: {offsetGridLines: false},
           scaleLabel: {
             display: (typeof settings.plainGraph === 'undefined' ? true : !settings.plainGraph),
             labelString: settings.xAxeDescription,
@@ -124,8 +216,13 @@ function printLineGraph(chartId, settings) {
               })()
             }
           },
+          ticks: {
+              // Need to be a variable (for rep_total_mail_by_date.php and rep_previous_day.php)
+              // auto or a number
+              //maxTicksLimit: 10
+          },
           type: 'category',
-        }]
+       }]
       },
       responsive: false,
       tooltips: {
@@ -154,7 +251,7 @@ function printLineGraph(chartId, settings) {
                 }
               }
             }
-            //COLON specified on main page via php __('colon99')
+            // COLON specified on main page via php __('colon99')
             var tooltipOutput = " " + settings.yAxeDescriptions[axisId] + COLON + " " + formattedData;
 
             return tooltipOutput;
