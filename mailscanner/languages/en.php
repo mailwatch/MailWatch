66e48ef1f897d36f093356bf8ce740b9819dbedd
Michael Pietsch <michael@mi-pietsch.de>
2017-11-29 17:49:35 +0100

    fix user selectable japanese lang

--- mailscanner/languages/en.php
+++ mailscanner/languages/en.php
@@ -1002,6 +1002,7 @@ return array(
     'en' => 'English',
     'fr' => 'Français',
     'it' => 'Italiano',
+    'ja' => 'Japanese',
     'nl' => 'Nederlands',
     'pt_br' => 'Português',
     'dbconnecterror99' => '<p>Error: Database connection failed</p><p>It is possible that the database is overloaded or otherwise not running properly</p><p class="emphasise">Contact the system administrator if the problem persists</p>',
