6dd5cae12b61223e759cfd172f147820905d1c1f
Michael Pietsch <michael@mi-pietsch.de>
2018-01-16 17:51:08 +0100

    fix merge+licence headers+style fixes

--- src/MailWatch/Entity/User.php
+++ src/MailWatch/Entity/User.php
@@ -1,11 +1,35 @@
 <?php
 
+/*
+ * MailWatch for MailScanner
+ * Copyright (C) 2003-2011  Steve Freegard (steve@freegard.name)
+ * Copyright (C) 2011  Garrod Alwood (garrod.alwood@lorodoes.com)
+ * Copyright (C) 2014-2018  MailWatch Team (https://github.com/mailwatch/1.2.0/graphs/contributors)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * In addition, as a special exception, the copyright holder gives permission to link the code of this program with
+ * those files in the PEAR library that are licensed under the PHP License (or with modified versions of those files
+ * that use the same license as those files), and distribute linked combinations including the two.
+ * You must obey the GNU General Public License in all respects for all of the code used other than those files in the
+ * PEAR library that are licensed under the PHP License. If you modify this program, you may extend this exception to
+ * your version of the program, but you are not obligated to do so.
+ * If you do not wish to do so, delete this exception statement from your version.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 namespace MailWatch\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\EquatableInterface;
-
 
 /**
  * @ORM\Entity(repositoryClass="MailWatch\Repository\UserRepository")
@@ -158,7 +182,7 @@ class User implements UserInterface, EquatableInterface
 
     public function setType($type)
     {
-        if(!in_array($type, ['A','D','U','R','H'])) {
+        if (!in_array($type, ['A','D','U','R','H'])) {
             throw new \InvalidArgumentException();
         }
         $this->type = $type;
@@ -201,7 +225,7 @@ class User implements UserInterface, EquatableInterface
 
     public function getRoles()
     {
-        return array("ROLE_".$this->type); //TODO: roles have to start with ROLE_ change, mirgrate db roles
+        return ["ROLE_".$this->type]; //TODO: roles have to start with ROLE_ change, migrate db roles
     }
 
     public function getSalt()
